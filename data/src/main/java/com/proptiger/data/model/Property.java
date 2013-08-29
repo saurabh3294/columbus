287f5ff578cc4aad6e0c908cb17da08ec26783f3
Mukand Agarwal <mukand.agarwal@proptiger.com>
2013-08-29 13:40:08 +0530

    adding Solr Property attributes in Property Class.

--- data/src/main/java/com/proptiger/data/model/Property.java
+++ data/src/main/java/com/proptiger/data/model/Property.java
@@ -8,16 +8,50 @@ import org.codehaus.jackson.annotate.JsonProperty;
 @JsonAutoDetect(fieldVisibility=Visibility.NONE, getterVisibility=Visibility.NONE, isGetterVisibility=Visibility.NONE)
 public class Property {
     private String id;
-    
+    private Long typeId;
+    private Long bedrooms;
+    private long bathrooms;
+    private String unitType;
     @Field(value="UNIT_NAME")
     @JsonProperty(value="UNIT_NAME")
     private String unitName;
-
+    private String projectType;
+    private Float pricePerUnitArea;
+    private Float size;
+    private Float budget;
+    private String price;
+    private String propertyUrl;
+    private Float northEastLatitude;
+    private Float northEastLongitude;
+    private Float southWestLatitude;
+    private Float southWestLongitude;
+    private Float centerLatitude;
+    private Float centerLongitude;
+    private String localityLabelPriority;
+    private String builderLabelPriority;
+    private String suburbLabelPriority;
+    private String projectStatusbedroom;
+    
+    
     public String getId() {
         return id;
     }
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    /**
+     * @return the unitName
+     */
+    public String getUnitName() {
+        return unitName;
+    }
+
+    /**
+     * @param unitName the unitName to set
+     */
+    public void setUnitName(String unitName) {
+        this.unitName = unitName;
     }
 }
