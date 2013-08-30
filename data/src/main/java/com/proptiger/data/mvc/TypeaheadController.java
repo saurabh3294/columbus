26e25285c46541b88432ed3266fc9b3d4c31da66
Mukand Agarwal <mukand.agarwal@proptiger.com>
2013-08-30 19:18:30 +0530

    working typeahead code.

--- data/src/main/java/com/proptiger/data/mvc/TypeaheadController.java
+++ data/src/main/java/com/proptiger/data/mvc/TypeaheadController.java
@@ -6,12 +6,12 @@ package com.proptiger.data.mvc;
 
 import com.proptiger.data.model.Typeahead;
 import com.proptiger.data.service.TypeAheadService;
-import java.awt.print.Pageable;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -23,9 +23,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class TypeaheadController {
     TypeAheadService typeaheadService = new TypeAheadService();
     
-    @RequestMapping(params = "{query}", method= RequestMethod.GET)
+    @RequestMapping
     @ResponseBody
-    public List<Typeahead> getSearchTypeahead(@PathVariable String query){
+    public List<Typeahead> getSearchTypeahead(@RequestParam String query){
         return typeaheadService.getSearchTypeahead(query);
     }
     
