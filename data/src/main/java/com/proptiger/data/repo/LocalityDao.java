6bbc4fe55e85472d04434aed998941f6fcf89735
Mukand Agarwal <mukand.agarwal@proptiger.com>
2013-09-13 00:41:47 +0530

    seperating enquiry into three seperate queries. Then finalizing the code is left.

--- data/src/main/java/com/proptiger/data/repo/LocalityDao.java
+++ data/src/main/java/com/proptiger/data/repo/LocalityDao.java
@@ -21,32 +21,21 @@ import org.springframework.stereotype.Repository;
 
 public interface LocalityDao extends JpaRepository<Locality, Long>{
     
-    @Query("SELECT -1 AS localityId, '' AS label, COUNT(*) AS enquiryCount, 'TOTAL' AS queryType "
+    @Query("SELECT COUNT(*) "
             + " FROM Locality L join L.enquiry E WHERE L.localityId=E.localityId AND "
             + " UNIX_TIMESTAMP(E.createdDate) >= (UNIX_TIMESTAMP() - ?1) AND "
-            + " CASE ?2 WHEN 1 THEN L.localityId ELSE L.cityId END = ?3")
-    public List<Locality> findEnquiryCountOnCityOrSubOrLoc(@Param Long timediff, @Param Long location_type, @Param Long location_id);
+            + " CASE ?2 WHEN 1 THEN L.localityId WHEN 2 THEN L.suburbId ELSE L.cityId END = ?3")
+    public Integer findTotalEnquiryCountOnCityOrSubOrLoc(@Param Long timediff, @Param Long location_type, @Param Long location_id);
 //    
+    
+    @Query("SELECT L.localityId, L.label, COUNT(*) AS enquiryCount"
+            + " FROM Locality L join L.enquiry E WHERE L.localityId=E.localityId AND "
+            + " UNIX_TIMESTAMP(E.createdDate) >= (UNIX_TIMESTAMP() - ?1) AND "
+            + " CASE ?2 WHEN 1 THEN L.localityId WHEN 2 THEN L.suburbId ELSE L.cityId END = ?3 GROUP BY E.localityId ORDER BY enquiryCount DESC ")
+    public List<Locality> findEnquiryCountOnCityOrSubOrLoc(@Param Long timediff, @Param Long location_type, @Param Long location_id);
     
     @Query("SELECT L.localityId, L.label, E.name, E.email, E.id"
             + " FROM Locality L join L.enquiry E where L.localityId = E.localityId and L.localityId=?1")
-            public List<Locality> findEnquiryCountOnLoc(@Param Long localityId);//@Param("localityId") Integer localityId, @Param("months") Integer months);
-    /* WHERE "
-            + " L.LOCALITY_ID = E.LOCALITY_ID AND L.LOCALITY_ID = 100 AND YEAR(created_date) = YEAR(CURRENT_DATE - INTERVAL 1 MONTH) AND "
-            + " MONTH(created_date) = MONTH(CURRENT_DATE - INTERVAL 1 MONTH) ")*/
-    
+    public List<Locality> findEnquiryCountOnLoc(@Param Long localityId);//@Param("localityId") Integer localityId, @Param("months") Integer months);
+     
 }
-/*@Query("select localityId, label from Locality where localityId > ?1 and localityId < ?2 "
-                    + " UNION "
-                    + " select localityId, label from Locality where localityId IN (?1, ?2, 1) ")*/
-
-
-/*
- * + " UNION "
-            + " SELECT L.localityId, L.label, COUNT(*) AS enquiryCount, 'GROUP' AS queryType"
-            + " FROM Locality L join L.enquiry E WHERE L.localityId=E.localityId AND ?1 AND "
-            + "  YEAR(createdDate) = YEAR(CURRENT_DATE - INTERVAL ?2 MONTH) AND "
-            + "  MONTH(createdDate) = MONTH(CURRENT_DATE - INTERVAL ?2 MONTH) "
-            + " ")
- * 
- */
