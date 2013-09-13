37b02f66d51d6b7e2ef9d32b795620ae1d23d9e2
rajeev.pandey <rajeev.pandey@proptiger.com>
2013-09-13 10:54:04 +0530

    [#MOBILE-100] acode refactoring

--- data/src/main/java/com/proptiger/data/pojo/Paging.java
+++ data/src/main/java/com/proptiger/data/pojo/Paging.java
@@ -20,6 +20,33 @@ public class Paging {
 	public void setRows(int rows) {
 		this.rows = rows;
 	}
+	@Override
+	public String toString() {
+		return "Paging [start=" + start + ", rows=" + rows + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + rows;
+		result = prime * result + start;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Paging other = (Paging) obj;
+		if (rows != other.rows)
+			return false;
+		if (start != other.start)
+			return false;
+		return true;
+	}
 	
 	
 }
