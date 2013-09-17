aaf932eb872c4affac62f6ab83539a5c39e50c02
rajeev.pandey <rajeev.pandey@proptiger.com>
2013-09-17 14:29:49 +0530

    [#MOBILE-100] merging for blog news API

--- data/src/main/java/com/proptiger/data/repo/BlogNewsDao.java
+++ data/src/main/java/com/proptiger/data/repo/BlogNewsDao.java
@@ -3,6 +3,7 @@ package com.proptiger.data.repo;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.proptiger.data.model.WordpressPost;
 
--- data/src/main/java/com/proptiger/data/service/BlogNewsService.java
+++ data/src/main/java/com/proptiger/data/service/BlogNewsService.java
@@ -2,6 +2,7 @@ package com.proptiger.data.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.proptiger.data.model.WordpressPost;
