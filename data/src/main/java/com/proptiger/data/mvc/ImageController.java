dbf793a28692d73b80120166a8e8ee22c34e3d4d
Yugal Jindle <yugalmait@gmail.com>
2013-09-23 14:55:25 +0530

    Images now converting to jpg and watermarked

--- data/src/main/java/com/proptiger/data/mvc/ImageController.java
+++ data/src/main/java/com/proptiger/data/mvc/ImageController.java
@@ -1,15 +1,9 @@
 package com.proptiger.data.mvc;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,7 +12,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.proptiger.data.model.DomainObject;
 import com.proptiger.data.model.image.Image;
-import com.proptiger.data.model.image.ImageUpload;
 import com.proptiger.data.pojo.ProAPISuccessResponse;
 import com.proptiger.data.service.ImageService;
 
@@ -44,14 +37,7 @@ public class ImageController extends BaseController {
     
     @RequestMapping(method = RequestMethod.POST)
     public @ResponseBody Object putImages(@RequestParam("image") MultipartFile image) {
-    	File newFile = new File("/home/yugal/image_uploads/" + image.getOriginalFilename());
-    	try {
-    		if(imageService.isValidImage(image)) {
-    			image.transferTo(newFile);	
-    		}
-		} catch (IllegalStateException | IOException e) {
-			e.printStackTrace();
-		}
+    	imageService.uploadImage(image);
     	return super.filterFields(new ProAPISuccessResponse(), null);
     }
 }
--- data/src/main/java/com/proptiger/data/service/ImageService.java
+++ data/src/main/java/com/proptiger/data/service/ImageService.java
@@ -1,16 +1,26 @@
 package com.proptiger.data.service;
 
+import java.awt.AlphaComposite;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import javax.imageio.ImageIO;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.proptiger.data.model.DomainObject;
 import com.proptiger.data.model.image.Image;
 import com.proptiger.data.repo.ImageDao;
+import com.proptiger.data.util.PropertyReader;
 
 /**
  * @author yugal
@@ -18,11 +28,65 @@ import com.proptiger.data.repo.ImageDao;
  */
 @Service
 public class ImageService {
+	private static File tempDir;
+	
+    @Autowired
+    protected PropertyReader propertyReader;
+    
+	@PostConstruct
+	private void init() {
+		String path = propertyReader.getRequiredProperty("imageTempPath");
+		tempDir = new File(path);
+		if (!tempDir.exists()) {
+			tempDir.mkdir();
+		}
+	}
+
 	@Resource
 	private ImageDao imageDao;
 	
+	private boolean isValidImage(MultipartFile file) {
+		return (file.getSize() == 0)? false:true;
+	}
+	
+	private File convertToJPG(File image) throws IOException {
+		BufferedImage img = null;
+		img = ImageIO.read(image);
+		File jpg = File.createTempFile("imageJPG", ".jpg", tempDir);
+		ImageIO.write(img, "jpg", jpg); // Writes at 0.7 compression quality
+		return jpg;
+	}
+
+	private void makeProgresiveJPG(File jpgFile) {
+	}
+	
+	private File createWatermarkedCopy(File jpgFile) throws IOException {
+		InputStream waterMarkFile = ImageService.class.getResourceAsStream("/com/proptiger/data/service/watermark.png");
+		BufferedImage waterMark = ImageIO.read(waterMarkFile);
+
+		BufferedImage image = ImageIO.read(jpgFile);
+		Graphics2D g = image.createGraphics();
+		try {
+			g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.5f)); // 50% transparent
+			g.drawImage(waterMark, (image.getWidth() - waterMark.getWidth())/2, (image.getHeight() - waterMark.getHeight())/2, null);
+		}
+		finally {
+			g.dispose();
+		}
+		File waterMarkImageFile = File.createTempFile("imageWatermark", ".jpg", tempDir);
+		ImageIO.write(image, "jpg", waterMarkImageFile);
+		return waterMarkImageFile;
+	}
+	
+	private HashMap<String, String> getFileAttributes() {
+		return new HashMap<String, String>();
+	}
+	
+	private void uploadToS3() {
+	}
+	
 	/*
-	 * Method to retrieve images
+	 * Public method to get images
 	 * */
 	public List<Image> getImages(DomainObject object, String type, int objId) {
 		if(type == null) {
@@ -32,30 +96,21 @@ public class ImageService {
 		}
 	}
 	
-	public boolean isValidImage(MultipartFile file) {
-		if(file.getSize() == 0) {
-			return false;
-		} else {
-			return true;
+	/*
+	 * Public method to upload images
+	 * */
+	public void uploadImage(MultipartFile image) {
+    	try {
+    		File tempFile = File.createTempFile("image", ".tmp", tempDir);
+    		File jpgFile, waterMarkImageFile;
+    		if(isValidImage(image)) {
+    			image.transferTo(tempFile);
+    			jpgFile = convertToJPG(tempFile);
+    			makeProgresiveJPG(jpgFile);
+    			waterMarkImageFile = createWatermarkedCopy(jpgFile);
+    		}
+		} catch (IllegalStateException | IOException e) {
+			e.printStackTrace();
 		}
-	}
-	
-	public void convertToJPG() {
-	}
-	
-	public void optimizeJPG() {
-	}
-	
-	public void makeProgresiveJPG() {
-	}
-	
-	public void createWatermarkedCopy() {
-	}
-	
-	public HashMap<String, String> getFileAttributes() {
-		return new HashMap<String, String>();
-	}
-	
-	public void uploadToS3() {
 	}
 }
