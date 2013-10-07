21357164f879a20f4986cb6b9d5fae0ed10e50c0
Yugal Jindle <yugal.jindle@proptiger.com>
2013-10-07 20:49:27 +0530

    Watermarking fixed

--- data/src/main/java/com/proptiger/data/service/ImageService.java
+++ data/src/main/java/com/proptiger/data/service/ImageService.java
@@ -4,6 +4,7 @@ import java.awt.AlphaComposite;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
+import java.awt.image.IndexColorModel;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -69,9 +70,14 @@ public class ImageService {
 	}
 	
 	private BufferedImage resize(BufferedImage image, int width, int height) {
-		BufferedImage resized = new BufferedImage(width, height, image.getType());
-		Graphics2D g = resized.createGraphics();  
-        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);  
+		BufferedImage resized = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_INDEXED, (IndexColorModel) image.getColorModel());
+		Graphics2D g = resized.createGraphics();
+		// Optimizations
+        g.setComposite(AlphaComposite.Src);
+    	g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+    	g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+    	g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        // Draw
         g.drawImage(image, 0, 0, width, height, 0, 0, image.getWidth(), image.getHeight(), null);  
         g.dispose();
         return resized;
