2aa9ea8cef4112c931e3a825fe64919da674d9d8
rajeev.pandey <rajeev.pandey@proptiger.com>
2013-10-08 17:45:10 +0530

    [#WEB-603] created models for portfolio property and API for type ahead based on tyahead type

---
+++ data/src/main/java/com/proptiger/data/model/portfolio/LoanStatus.java
@@ -0,0 +1,9 @@
+package com.proptiger.data.model.portfolio;
+
+/**
+ * @author Rajeev Pandey
+ *
+ */
+public enum LoanStatus {
+	AVAILED,NOT_AVAILED,PAID;
+}
--- data/src/main/java/com/proptiger/data/mvc/portfolio/PaymentPlanController.java
+++ data/src/main/java/com/proptiger/data/mvc/portfolio/PaymentPlanController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "data/v1/entity/user/{userId}/payment-plan")
+@RequestMapping(value = "data/v1/entity/user/{userId}/payment-plan/{projectId}/{planType}/")
 public class PaymentPlanController {
 
 }
