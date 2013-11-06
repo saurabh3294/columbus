e744096cfdcd8a88f81711ea2f2706306e3c4f5f
rajeev.pandey <rajeev.pandey@proptiger.com>
2013-11-06 16:37:27 +0530

    [#WEB-603] Update for lead page name and additional fields in portfolio listing

---
+++ data/src/main/java/com/proptiger/data/service/portfolio/LeadPageName.java
@@ -0,0 +1,24 @@
+package com.proptiger.data.service.portfolio;
+
+/**
+ * @author Rajeev Pandey
+ *
+ */
+public enum LeadPageName {
+	PORTFOLIO("portfolio"),
+	NO_NAME("");
+	
+	private LeadPageName(String s){
+		this.name = s;
+	}
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
