09d1a606dc0a88cceffff89d87d852581422f688
Hemendra <hemendra.srivastava@proptiger.com>
2013-11-18 15:02:39 +0530

    changes to typeahead codebase

--- data/src/main/java/com/proptiger/data/mvc/TypeaheadController.java
+++ data/src/main/java/com/proptiger/data/mvc/TypeaheadController.java
@@ -4,22 +4,31 @@
  */
 package com.proptiger.data.mvc;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.common.base.Joiner;
+import com.proptiger.data.model.City;
 import com.proptiger.data.model.Typeahead;
 import com.proptiger.data.pojo.ProAPIResponse;
 import com.proptiger.data.pojo.ProAPISuccessCountResponse;
 import com.proptiger.data.service.TypeaheadService;
+import com.proptiger.data.service.portfolio.CityService;
+import com.proptiger.data.service.portfolio.DashboardService;
 
 /**
  *
  * @author mukand
+ * @author hemendra
+ * 
  */
 @Controller
 @RequestMapping(value="app/v1/typeahead")
@@ -27,15 +36,75 @@ public class TypeaheadController extends BaseController {
     @Autowired
     TypeaheadService typeaheadService;
     
+    @Autowired
+    CityService cityService;
+    
+    private static Logger logger = LoggerFactory.getLogger(DashboardService.class);
+    
 	@RequestMapping
 	@ResponseBody
 	public ProAPIResponse getTypeaheads(@RequestParam String query,
 			@RequestParam(defaultValue = "5") int rows,
 			@RequestParam(required = false) String typeAheadType,
 			@RequestParam(required = false) String city) {
-		List<Typeahead> list = typeaheadService.getTypeaheads(query, rows, typeAheadType, city);
+		
+		List<String> filterQueries = new ArrayList<String>();
+		if (typeAheadType != null && typeAheadType.trim() != "")
+			filterQueries.add("TYPEAHEAD_TYPE:"+typeAheadType);
+		
+		if (city != null && city.trim() != "") {
+			filterQueries.add("TYPEAHEAD_CITY:"+city);
+		}
+		else {
+			List<String> cityList = this.findCities(query);
+			if (!cityList.isEmpty()) {
+				query = this.parseCities(query, cityList);
+				String fqCity = Joiner.on(" OR TYPEAHEAD_CITY:").skipNulls().join(cityList);
+			
+				if (fqCity.trim() != "")
+					filterQueries.add("TYPEAHEAD_CITY:"+fqCity.trim());
+			}
+		}
+		
+		List<Typeahead> list = typeaheadService.getTypeaheads(query, rows, filterQueries);
 		return new ProAPISuccessCountResponse(super.filterFields(list, null), list.size());
 	}
-    
-   
+	
+	private String parseCities(String query, List<String> queryCities) {		
+		String query_new = this.substituteQuery(query, queryCities);
+		
+		if (query_new != "")
+			return query_new;
+		else
+			return query;
+	}
+	
+	private List<String> findCities(String query) {
+		// TODO Auto-generated method stub
+		List<City> cityList = cityService.getCityList(null);
+		List<String> cityLabels = new ArrayList<String>();
+		
+		for (City city : cityList) {
+			String label = city.getLabel();
+			if (label != "")
+				cityLabels.add(city.getLabel());
+		}
+
+		List<String> matchedCities = new ArrayList<String>();
+		for (String city: cityLabels) {
+			if (query.matches("(?i).*"+city+".*")) {
+				if (city != "")
+					matchedCities.add(city);
+			}
+		}
+		return matchedCities;
+	}
+
+	private String substituteQuery(String query, List<String> terms) {
+		// replaces terms in query with terms
+		for (String term : terms) {
+			query = query.replace(term.toLowerCase(), "");
+		}
+		return query.trim();
+	}
 }
--- data/src/main/java/com/proptiger/data/repo/TypeaheadDao.java
+++ data/src/main/java/com/proptiger/data/repo/TypeaheadDao.java
@@ -1,53 +1,118 @@
 
 package com.proptiger.data.repo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.solr.client.solrj.SolrQuery;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.google.common.base.Joiner;
 import com.proptiger.data.model.Typeahead;
+import com.proptiger.data.service.portfolio.DashboardService;
 
 /**
  * 
  * @author mukand
+ * @author hemendra
+ * 
  */
+
+
 @Repository
 public class TypeaheadDao {
-
+	
+	private static Logger logger = LoggerFactory.getLogger(DashboardService.class);
+	
     @Autowired
     private SolrDao solrDao;
+    
+	private SolrQuery getSolrQuery(String query, int rows, List<String> filterQueries) {
+		SolrQuery solrQuery = this.getQueryParams(query);
+        
+        for (String fq : filterQueries) {
+        	solrQuery.addFilterQuery(fq);
+		}
 
-    public List<Typeahead> getTypeaheads(String query, int rows,  String typeAheadType, String cityName) {
-        SolrQuery solrQuery = getSolrQuery(query, rows, typeAheadType, cityName);
-        return solrDao.executeQuery(solrQuery).getBeans(Typeahead.class);
-    }
-
-	private SolrQuery getSolrQuery(String query, int rows, String typeAheadType, String cityName) {
-		SolrQuery solrQuery = new SolrQuery();
-
-        solrQuery.setQuery(getQueryParams(query));
-        solrQuery.setFilterQueries("DOCUMENT_TYPE:TYPEAHEAD");
-        if(typeAheadType != null && !"".equals(typeAheadType)){
-        	 solrQuery.addFilterQuery("TYPEAHEAD_TYPE:"+typeAheadType.toUpperCase());
-        }
-        if(cityName != null && !"".equals(cityName)){
-        	solrQuery.addFilterQuery("TYPEAHEAD_CITY:"+cityName.toLowerCase());
-        }
-        solrQuery.setRows(rows);
+		solrQuery.setRows(rows);
 		return solrQuery;
 	}
-    
-    private String getQueryParams(String query) {
-        String queryParams = new String();
-        queryParams += "TYPEAHEAD_CORE_TEXT_NGRAMS:" + query + " ";
-        queryParams += " OR TYPEAHEAD_LABEL_NGRAMS(" + query + ") ";
-        queryParams += " OR TYPEAHEAD_CORE_TEXT:(" + query + ")^20 ";
-        queryParams += " OR TYPEAHEAD_LABEL:(" + query + ")^20 ";
-        queryParams += " OR TYPEAHEAD_CORE_TEXT:(" + query + "~)^10 ";
-        queryParams += " OR TYPEAHEAD_LABEL:(" + query + "~)^10";
 
-        return queryParams;
-    }
+	private SolrQuery getQueryParams(String query) {
+		SolrQuery solrQuery = new SolrQuery();
+		List<String> queryTokens = this.tokenizeQuery(query);
+		String queryParams = new String();
+		double wt = 1.0;
+		if (query.trim().isEmpty())
+			wt = 1.0 / queryTokens.size();
+		
+		solrQuery.setQuery(query);
+		solrQuery.setParam("defType", "edismax");
+		solrQuery.setParam("qf", "tp_engram^5.0 tp_ngram^0.5 tp_phonetic^0.2");
+		
+		List<String> boostList = new ArrayList<String>();
+		for (String qry : queryTokens) {
+			if (qry.split(" ").length == 1)
+				boostList
+						.add("query({!edismax qf='tp_city tp_locality tp_builder tp_suburb tp_project' tie=0.1 v='$q' boost=''}, "+String.format("%.2f", wt)+")");
+			else
+				boostList
+						.add("query({!edismax qf='tp_city tp_locality tp_builder tp_suburb tp_project' tie=0.1 v='\"$q\"' boost=''}, $wt)");
+		}
+
+		String boost = new String();
+		if (boostList.size() > 1)
+			boost = "sum(" + Joiner.on(",").skipNulls().join(boostList) + ")";
+		else
+			boost = boostList.get(0);
+
+		boost = "product(map(query({!v='TYPEAHEAD_TYPE:CITY'}),0,0,1,1.5),map(query({!v='TYPEAHEAD_TYPE:BUILDER'}),0,0,1,0.8),map(query({!v='TYPEAHEAD_TYPE:PROJECT'}),0,0,1,1.3),map(query({!v='TYPEAHEAD_TYPE:LOCALITY'}),0,0,1,1.3),map(query({!v='TYPEAHEAD_TYPE:SUBURB'}),0,0,1,1.3),"
+				+ boost + ")";
+		
+		solrQuery.setParam("boost", boost);
+		
+		logger.debug(queryParams);
+		return solrQuery;
+	}
+
+	public List<List<String>> powerset(List<String> list) {
+		List<List<String>> ps = new ArrayList<List<String>>();
+	ps.add(new ArrayList<String>());
+
+		for (String item : list) {
+			List<List<String>> newPs = new ArrayList<List<String>>();
+
+			for (List<String> subset : ps) {
+				newPs.add(subset);
+				List<String> newSubset = new ArrayList<String>(subset);
+				newSubset.add(item);
+				newPs.add(newSubset);
+			}
+
+			ps = newPs;
+		}
+		return ps;
+	}
+
+	private List<String> tokenizeQuery(String query) {
+		List<String> qList = new ArrayList<String>();
+		List<List<String>> powerset = this.powerset(qList);
+		for (List<String> list : powerset) {
+			String st = Joiner.on(" ").skipNulls().join(list);
+			if (st.trim() != "" && st != null)
+				qList.add(st);
+		}
+		return qList;
+	}
+
+	public List<Typeahead> getTypeaheads(String query, int rows,
+			List<String> filterQueries) {
+		
+		SolrQuery solrQuery = this.getSolrQuery(query, rows, filterQueries);
+		logger.info(query);
+		return solrDao.executeQuery(solrQuery).getBeans(Typeahead.class);
+	}
 }
