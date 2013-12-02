df5fb0ca7b62c6cb08ee7eec2ac1119daf2e5ffa
Mukand Agarwal <mukand.agarwal@proptiger.com>
2013-12-02 19:04:25 +0530

    adding Project Secondary Price Table Model.

---
+++ data/src/main/java/com/proptiger/data/model/ProjectSecondaryPrice.java
@@ -0,0 +1,95 @@
+package com.proptiger.data.model;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.proptiger.data.meta.FieldMetaInfo;
+import com.proptiger.data.meta.ResourceMetaInfo;
+
+@Entity
+@Table(name="cms.project_secondary_price")
+@ResourceMetaInfo
+public class ProjectSecondaryPrice implements BaseModel{
+	@Id
+	@FieldMetaInfo(displayName = "Id", description = "Id")
+	@Column(name="ID")
+	private int Id;
+	
+	@Column(name="PROJECT_ID")
+	@FieldMetaInfo(displayName = "Project Id", description = "Project Id")
+	private int projectId;
+	
+	@Column(name="UNIT_TYPE")
+	@FieldMetaInfo(displayName = "Unit Type", description = "Unit Type")
+	private String unitType;
+	
+	@Column(name="MIN_PRICE")
+	@FieldMetaInfo(displayName = "Min Price", description = "Min Price")
+	private int minPrice;
+	
+	@Column(name="MAX_PRICE")
+	@FieldMetaInfo(displayName = "Max Price", description = "Max Price")
+	private int maxPrice;
+	
+	@FieldMetaInfo(displayName = "Effective Date", description = "Effective Date")
+	@Column(name="EFFECTIVE_DATE")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date effectiveDate;
+
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public int getProjectId() {
+		return projectId;
+	}
+
+	public void setProjectId(int projectId) {
+		this.projectId = projectId;
+	}
+
+	public String getUnitType() {
+		return unitType;
+	}
+
+	public void setUnitType(String unitType) {
+		this.unitType = unitType;
+	}
+
+	public int getMinPrice() {
+		return minPrice;
+	}
+
+	public void setMinPrice(int minPrice) {
+		this.minPrice = minPrice;
+	}
+
+	public int getMaxPrice() {
+		return maxPrice;
+	}
+
+	public void setMaxPrice(int maxPrice) {
+		this.maxPrice = maxPrice;
+	}
+
+	public Date getEffectiveDate() {
+		return effectiveDate;
+	}
+
+	public void setEffectiveDate(Date effectiveDate) {
+		this.effectiveDate = effectiveDate;
+	}
+	
+	
+
+}
