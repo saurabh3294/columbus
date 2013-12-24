3691a1b6a31a0ae86f33040b0df3696957a0f64c
rajeev.pandey <rajeev.pandey@proptiger.com>
2013-12-24 16:52:17 +0530

    [WEB-1026] code comment

--- data/src/main/java/com/proptiger/data/service/CityService.java
+++ data/src/main/java/com/proptiger/data/service/CityService.java
@@ -9,11 +9,22 @@ import com.proptiger.data.model.City;
 import com.proptiger.data.pojo.Selector;
 import com.proptiger.data.repo.CityDao;
 
+/**
+ * Service class to get city data
+ * 
+ * @author Rajeev Pandey
+ * 
+ */
 @Service
 public class CityService {
 	@Autowired
 	private CityDao cityDao;
 	
+	/**
+	 * Get list of city details
+	 * @param selector
+	 * @return
+	 */
 	public List<City> getCityList(Selector selector){
 		return cityDao.getCities(selector);
 	}
