e3cf4b7982386eb4213902bac7eff8f4d02765ad
rajeev.pandey <rajeev.pandey@proptiger.com>
2014-01-07 17:08:53 +0530

    [#MIDL-11] agent by project id API

--- data/src/main/java/com/proptiger/data/mvc/BrokerAgentController.java
+++ data/src/main/java/com/proptiger/data/mvc/BrokerAgentController.java
@@ -1,5 +1,7 @@
 package com.proptiger.data.mvc;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +12,7 @@ import com.proptiger.data.meta.DisableCaching;
 import com.proptiger.data.model.seller.Agent;
 import com.proptiger.data.pojo.ProAPIResponse;
 import com.proptiger.data.pojo.ProAPISuccessCountResponse;
+import com.proptiger.data.pojo.ProAPISuccessResponse;
 import com.proptiger.data.service.BrokerAgentService;
 
 /**
@@ -17,17 +20,22 @@ import com.proptiger.data.service.BrokerAgentService;
  *
  */
 @Controller
-@RequestMapping(value = "data/v1/entity/broker-agent")
-@DisableCaching
 public class BrokerAgentController extends BaseController{
 	
 	@Autowired
-	private BrokerAgentService sellerService;
+	private BrokerAgentService brokerAgentService;
 	
-	@RequestMapping(value = "{agentId}")
+	@RequestMapping(value = "data/v1/entity/broker-agent/{agentId}")
 	@ResponseBody
 	public ProAPIResponse getAgent(@PathVariable Integer agentId){
-		Agent agent = sellerService.getAgent(agentId);
-		return new ProAPISuccessCountResponse(super.filterFieldsWithTree(agent, null), 1);
+		Agent agent = brokerAgentService.getAgent(agentId);
+		return new ProAPISuccessCountResponse(super.filterFields(agent, null), 1);
+	}
+	
+	@RequestMapping(value = "data/v1/entity/project/{projectId}/agent")
+	@ResponseBody
+	public ProAPIResponse getAgenetsForProject(@PathVariable Integer projectId){
+		List<Agent> agents = brokerAgentService.getAgentsForProject(projectId);
+		return new ProAPISuccessCountResponse(super.filterFields(agents, null), agents.size());
 	}
 }
--- data/src/main/java/com/proptiger/data/repo/seller/RuleAgentMappingDao.java
+++ data/src/main/java/com/proptiger/data/repo/seller/RuleAgentMappingDao.java
@@ -3,6 +3,7 @@ package com.proptiger.data.repo.seller;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.proptiger.data.model.seller.RuleAgentMapping;
 
@@ -12,4 +13,8 @@ import com.proptiger.data.model.seller.RuleAgentMapping;
  */
 public interface RuleAgentMappingDao extends JpaRepository<RuleAgentMapping, Integer>{
 	public List<RuleAgentMapping> findByAgentId(Integer agentId); 
+	
+	@Query("select agentId from RuleAgentMapping RA where RA.ruleId in ?1")
+	public List<Integer> findAgentsIdsWhereRuleIdsIn(List<Integer> ruleIds);
+	
 }
