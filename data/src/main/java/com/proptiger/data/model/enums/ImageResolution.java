77ebce8ef6e582c5a11de85dc164bdd2d640265e
Mandeep Dhir <mandeep.dhir@proptiger.com>
2014-01-17 20:44:48 +0530

    Changes as suggested by UX team

--- data/src/main/java/com/proptiger/data/model/enums/ImageResolution.java
+++ data/src/main/java/com/proptiger/data/model/enums/ImageResolution.java
@@ -12,9 +12,9 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 @JsonFormat(shape=JsonFormat.Shape.OBJECT)
 public enum ImageResolution {
     THUMBNAIL(130, 100),
-    MEDIUM(320, 240),
-    LARGE(940, 720),
-    VERY_LARGE(1040, 780),
+    SMALL(320, 240),
+    MEDIUM(520, 400),
+    LARGE(1336, 768),
     MOBILE_0(220, 120),
     MOBILE_1(280, 200),
     MOBILE_2(320, 220),
@@ -22,8 +22,9 @@ public enum ImageResolution {
     MOBILE_4(420, 280),
     MOBILE_5(480, 320),
     MOBILE_6(520, 340),
+    MOBILE_7(1040, 780),
     MOBILE_8(380, 280),
-    MOBILE_9(520, 400),
+    MOBILE_9(940, 720),
     MOBILE_10(680, 580),
     MOBILE_11(800, 620),
     MOBILE_12(940, 720),
