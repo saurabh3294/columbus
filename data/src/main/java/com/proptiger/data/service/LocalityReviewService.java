690ff48ab7b00ca43bc002f40c6a1b18ee40c46f
Mukand Agarwal <mukand.agarwal@proptiger.com>
2014-01-29 16:25:04 +0530

    adding imports.

--- data/src/main/java/com/proptiger/data/service/LocalityReviewService.java
+++ data/src/main/java/com/proptiger/data/service/LocalityReviewService.java
@@ -9,6 +9,8 @@ import javax.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +22,7 @@ import com.proptiger.data.model.LocalityReviewComments.LocalityReviewRatingDetai
 import com.proptiger.data.pojo.LimitOffsetPageRequest;
 import com.proptiger.data.pojo.Selector;
 import com.proptiger.data.repo.LocalityReviewDao;
+import com.proptiger.data.util.Constants;
 
 
 /**
