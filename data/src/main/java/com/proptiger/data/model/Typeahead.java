4cbc6aec54ac5961ae53a1087b250bdb20d53b2a
Nikhil Singhal <nikhil.singhal@proptiger.com>
2014-02-13 19:01:02 +0530

    [#MIDL-105] Adding SerialversionUID to Typehead Entity

--- data/src/main/java/com/proptiger/data/model/Typeahead.java
+++ data/src/main/java/com/proptiger/data/model/Typeahead.java
@@ -27,7 +27,7 @@ public class Typeahead extends BaseModel {
 	private static final long serialVersionUID = 2096261268711516512L;
 
 	@Field(value="id")
-    private String id;
+    private String id;	
     
     @Field(value="TYPEAHEAD_LABEL")
     private String label;
@@ -57,11 +57,8 @@ public class Typeahead extends BaseModel {
     @Field(value="TYPEAHEAD_CORE_TEXT_NGRAMS")
     private String coreTextNgrams;
     
-    @Field(value="LATITUDE")
-    private Double latitude;
-    
-    @Field(value="LONGITUDE")
-    private Double longitude;
+    @Field(value="LOCALITY_URL")
+    private String localityURL;
 
     public String getId() {
         return id;
@@ -143,19 +140,11 @@ public class Typeahead extends BaseModel {
         this.coreTextNgrams = coreTextNgrams;
     }
 
-    public Double getLatitude() {
-        return latitude;
+    public String getLocalityURL() {
+        return localityURL;
     }
 
-    public void setLatitude(Double latitude) {
-        this.latitude = latitude;
-    }
-
-    public Double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(Double longitude) {
-        this.longitude = longitude;
+    public void setLocalityURL(String localityURL) {
+        this.localityURL = localityURL;
     }
 }
