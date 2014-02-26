15580bbe3b8890bd27a67a46cf03c8448574b56a
Mukand Agarwal <mukand.agarwal@proptiger.com>
2014-02-26 17:06:36 +0530

    id change.

--- data/src/main/java/com/proptiger/data/util/IdConverterForDatabase.java
+++ data/src/main/java/com/proptiger/data/util/IdConverterForDatabase.java
@@ -14,35 +14,39 @@ import com.proptiger.data.model.enums.DomainObject;
 public class IdConverterForDatabase {
 
     public static Integer convertProjectIdFromCMSToProptiger(Property property) {
-        Integer projectId = 0;
+        return property.getPropertyId();
+        /*Integer projectId = 0;
         if (property != null && property.getProjectId() > DomainObject.project.getStartId()) {
             projectId = property.getProjectId() - DomainObject.project.getStartId();
         }
-        return projectId;
+        return projectId;*/
     }
 
     public static Integer convertPropertyIdFromCMSToProptiger(Integer propertyId) {
-        Integer typeId = 0;
+        return propertyId;
+        /*Integer typeId = 0;
         if (propertyId != null && propertyId > DomainObject.property.getStartId()) {
             typeId = propertyId - DomainObject.property.getStartId();
         }
-        return typeId;
+        return typeId;*/
     }
 
     public static int getProptigerDomainIdForDomainTypes(DomainObject domainObject, int id) {
-        int startId = domainObject.getStartId();
+        return id;
+        /*int startId = domainObject.getStartId();
         if (id > startId) {
             return id - startId;
         }
-        return id;
+        return id;*/
     }
 
     public static int getCMSDomainIdForDomainTypes(DomainObject domainObject, int id) {
-        int startId = domainObject.getStartId();
+        return id;
+        /*int startId = domainObject.getStartId();
         if (id < startId) {
             return id + startId;
         }
-        return id;
+        return id;*/
     }
 
 }
