46332252d90a089bbf747b07f879be36a76f69d4
Mukand Agarwal <mukand.agarwal@proptiger.com>
2014-04-09 01:01:58 +0530

    adding select for backing image ids which are being made inactive.

--- imageMigration/sql/duplicateBuilders.sql
+++ imageMigration/sql/duplicateBuilders.sql
@@ -1,4 +1,6 @@
 
+select I.id  from  Image I JOIN ImageType IT ON (I.imagetype_id=IT.id) JOIN ( select Im.original_hash, OTm.id from Image Im JOIN ImageType ITm ON (Im.imagetype_id=ITm.id) JOIN ObjectType OTm ON (ITm.objecttype_id=OTm.id AND OTm.type="builder") where Im.active = 1 group by Im.original_hash, OTm.id having count(*) > 5 ) T ON (I.original_hash = T.original_hash and IT.objecttype_id = T.id)  INTO OUTFILE '/tmp/duplicateBuilders.csv' ;
+
 /* deleting duplicate images across builders.*/
 update Image I JOIN ImageType IT ON (I.imagetype_id=IT.id) JOIN ( select Im.original_hash, OTm.id from Image Im JOIN ImageType ITm ON (Im.imagetype_id=ITm.id) JOIN ObjectType OTm ON (ITm.objecttype_id=OTm.id AND OTm.type="builder") where Im.active = 1 group by Im.original_hash, OTm.id having count(*) > 5 ) T ON (I.original_hash = T.original_hash and IT.objecttype_id = T.id) set I.active = 0;
 
