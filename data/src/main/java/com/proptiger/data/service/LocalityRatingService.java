06a00e930bd6c2d4d0b12162efd46f80e08d35cc
rajeev.pandey <rajeev.pandey@proptiger.com>
2014-04-08 17:33:45 +0530

    [#WEB-2236] rating count in locality

--- data/src/main/java/com/proptiger/data/service/LocalityRatingService.java
+++ data/src/main/java/com/proptiger/data/service/LocalityRatingService.java
@@ -113,6 +113,8 @@ public class LocalityRatingService {
             Constants.CacheName.LOCALITY_RATING_USERS }, key = "#localityId")
     @Transactional(rollbackFor = { ConstraintViolationException.class })
     public LocalityRatings createLocalityRating(Integer userId, Integer localityId, LocalityRatings localityRatings) {
+        //TODO in case of multiple request from same user and same locality this method creates two row in database
+        //TODO need to prevent this
         logger.debug("create locality rating for user {} locality {}", userId, localityId);
         LocalityRatings created = null;
         localityRatings.setLocalityId(localityId);
