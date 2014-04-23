8689c0dc4e2760470ccc91b4d7950a5554250e99
Mukand Agarwal <mukand.agarwal@proptiger.com>
2014-04-23 12:44:37 +0530

    MIDL-218

--- data/src/main/java/com/proptiger/data/mvc/LandMarkController.java
+++ data/src/main/java/com/proptiger/data/mvc/LandMarkController.java
@@ -20,12 +20,12 @@ import com.proptiger.data.service.LandMarkService;
  * @author mukand
  */
 @Controller
-@RequestMapping(value = "data/v1/entity/locality")
+@RequestMapping(value = "data/v1/entity/")
 public class LandMarkController extends BaseController {
     @Autowired
     private LandMarkService localityAmenityService;
 
-    @RequestMapping(value = "{id}/amenity", method = RequestMethod.GET)
+    @RequestMapping(value = "locality/{id}/amenity", method = RequestMethod.GET)
     @ResponseBody
     public Object getAmenitiesByLocalityIdAndAmenity(@PathVariable("id") int localityId, @RequestParam(
             value = "amenity",
