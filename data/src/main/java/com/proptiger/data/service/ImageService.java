dd94e262e88633dc415b29a26d9faf862f8d44da
Mukand Agarwal <mukand.agarwal@proptiger.com>
2014-04-25 17:40:35 +0530

    MIDL-2076 , converting all the image to sRGB encoding.

--- data/src/main/java/com/proptiger/data/service/ImageService.java
+++ data/src/main/java/com/proptiger/data/service/ImageService.java
@@ -101,6 +101,7 @@ public class ImageService {
             imOps.quality(95.0);
             imOps.interlace("Plane");
             imOps.addImage();
+
             MogrifyCmd command = new MogrifyCmd();
             command.run(imOps, outputFile.getAbsolutePath());
         }
@@ -245,6 +246,15 @@ public class ImageService {
             // Image uploaded
             File processedFile = File.createTempFile("processedImage", Image.DOT + format, tempDir);
             Files.copy(originalFile, processedFile);
+            
+            // Converting the image to RGB format.
+            try {
+                File rgbFile = convertToRGB(processedFile, format);
+                Files.copy(rgbFile, processedFile);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
 
             if (addWaterMark) {
                 applyWaterMark(processedFile, format);
@@ -254,12 +264,13 @@ public class ImageService {
 
             Image duplicateImage = isImageHashExists(originalHash, objectId, object.getText());
             if (duplicateImage != null)
-                throw new ResourceAlreadyExistException(
-                        "This Image Already Exists for "+object.getText()+" id-" + duplicateImage.getObjectId()
-                                + " with image id-"
-                                + duplicateImage.getId()
-                                + " under the category of "
-                                + duplicateImage.getImageTypeObj().getType());
+                throw new ResourceAlreadyExistException("This Image Already Exists for " + object.getText()
+                        + " id-"
+                        + duplicateImage.getObjectId()
+                        + " with image id-"
+                        + duplicateImage.getId()
+                        + " under the category of "
+                        + duplicateImage.getImageTypeObj().getType());
 
             // Persist
             Image image = imageDao.insertImage(
@@ -286,8 +297,8 @@ public class ImageService {
         deleteImageInCache(id);
         imageDao.setActiveFalse(id);
     }
-    
-    @Cacheable(value = Constants.CacheName.CACHE, key="'imageId:'+#id")
+
+    @Cacheable(value = Constants.CacheName.CACHE, key = "'imageId:'+#id")
     public Image getImage(long id) {
         return imageDao.findOne(id);
     }
@@ -302,7 +313,7 @@ public class ImageService {
         String keys[] = new String[3];
         keys[0] = object.getText() + imageTypeStr + objectId;
         keys[1] = object.getText() + "null" + objectId;
-        keys[2] = "imageId:"+imageId;
+        keys[2] = "imageId:" + imageId;
 
         return keys;
     }
@@ -326,4 +337,17 @@ public class ImageService {
 
         return imageIds.get(0);
     }
+
+    private File convertToRGB(File imageFile, String format) throws Exception {
+        ConvertCmd convertCmd = new ConvertCmd();
+        IMOperation imOperation = new IMOperation();
+        imOperation.addImage(imageFile.getAbsolutePath());
+        imOperation.colorspace("sRGB");
+        File outputFile = File.createTempFile("rgbImage", Image.DOT + format, tempDir);
+        imOperation.addImage(outputFile.getAbsolutePath());
+        convertCmd.run(imOperation);
+
+        return outputFile;
+    }
+
 }
