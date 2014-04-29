feca4b763ea4adecec61a9403e61bb8ecf535437
rajeev.pandey <rajeev.pandey@proptiger.com>
2014-04-29 13:49:47 +0530

    [#MIDL-1] clear cache if no name provided

--- data/src/main/java/com/proptiger/cache/mvc/CacheController.java
+++ data/src/main/java/com/proptiger/cache/mvc/CacheController.java
@@ -6,29 +6,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.proptiger.data.meta.DisableCaching;
 import com.proptiger.data.pojo.ProAPIResponse;
 import com.proptiger.data.pojo.ProAPISuccessResponse;
 import com.proptiger.data.util.Caching;
 
 @Controller
 @RequestMapping(value = "cache")
+@DisableCaching
 public class CacheController {
 
-    @Autowired
-    private Caching caching;
-    
-    @RequestMapping(value = "/clear-cache")
-    @ResponseBody
-    public ProAPIResponse clearCache(@RequestParam String cacheName ) {
-        StringBuilder cacheCleared = new StringBuilder();
-        if(!cacheName.isEmpty()){
-            String[] caches = cacheName.split(",");
-            for(String cache: caches){
-                caching.deleteMultipleResponseFromCacheOnRegex("*", cache);
-                cacheCleared.append(cache).append(",");
-            }
-        }
-        return new ProAPISuccessResponse("Cache Cleared: "+cacheCleared.toString());
-    }
+	@Autowired
+	private Caching caching;
+
+	@RequestMapping(value = "/clear-cache")
+	@ResponseBody
+	public ProAPIResponse clearCache(
+			@RequestParam(defaultValue = "") String cacheName) {
+		StringBuilder cacheCleared = new StringBuilder();
+		if("".equals(cacheName)){
+			cacheCleared.append("All");
+		}
+		String[] caches = cacheName.split(",");
+		for (String cache : caches) {
+			caching.deleteMultipleResponseFromCacheOnRegex("*", cache);
+			cacheCleared.append(cache).append(",");
+		}
+		return new ProAPISuccessResponse("Cleared Cache Names: "
+				+ cacheCleared.toString());
+	}
 
 }
