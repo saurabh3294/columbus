85134d076dc68df56830a5b4a704f4879e3cb355
Mukand Agarwal <mukand.agarwal@proptiger.com>
2014-06-09 19:04:59 +0530

    sending the response in the model

--- data/src/main/java/com/proptiger/data/mvc/UrlController.java
+++ data/src/main/java/com/proptiger/data/mvc/UrlController.java
@@ -1,7 +1,5 @@
 package com.proptiger.data.mvc;
 
-import java.io.IOException;
-
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,12 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-import com.google.gson.Gson;
 import com.proptiger.data.pojo.response.APIResponse;
 import com.proptiger.data.service.URLService;
-import com.proptiger.data.service.URLService.ValidURLResponse;
 
 @Controller
 @RequestMapping
@@ -24,15 +19,8 @@ public class UrlController extends BaseController {
 
     @RequestMapping("data/v1/url")
     @ResponseBody
-    public APIResponse getProjects(@RequestParam String url, HttpServletResponse response) throws IOException {
-
-        ValidURLResponse validURLResponse = urlService.getURLStatus(url);
-        
-        response.setStatus(validURLResponse.getHttpStatus());
-        if (validURLResponse.getRedirectUrl() != null) {
-            response.sendRedirect("/" + validURLResponse.getRedirectUrl());
-        }
-        return new APIResponse();
+    public APIResponse getProjects(@RequestParam String url, HttpServletResponse response) {
+        return new APIResponse(urlService.getURLStatus(url));
     }
 
 }
