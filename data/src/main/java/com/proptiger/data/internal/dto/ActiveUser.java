38433f964d80f9585d6c2e6ae336f86092be8fe8
Rajeev Pandey <rajeev.pandey@proptiger.com>
2014-06-13 17:29:51 +0530

    [#MIDL-19] removing spring security and enabling memcache based

--- data/src/main/java/com/proptiger/data/internal/dto/ActiveUser.java
+++ data/src/main/java/com/proptiger/data/internal/dto/ActiveUser.java
@@ -1,5 +1,6 @@
 package com.proptiger.data.internal.dto;
 
+import java.io.Serializable;
 import java.util.Collection;
 
 import org.springframework.security.core.GrantedAuthority;
@@ -14,13 +15,13 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
  * @author Rajeev Pandey
  * 
  */
-public class ActiveUser extends User {
+public class ActiveUser implements Serializable {
     private static final long serialVersionUID = -3022788419586557079L;
     private Integer           userIdentifier;
     @JsonIgnore
     public boolean            admin            = false;
 
-    public ActiveUser(
+    /*public ActiveUser(
             Integer id,
             String username,
             String password,
@@ -31,7 +32,7 @@ public class ActiveUser extends User {
             Collection<? extends GrantedAuthority> authorities) {
         super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
         this.userIdentifier = id;
-    }
+    }*/
 
     public Integer getUserIdentifier() {
         return userIdentifier;
