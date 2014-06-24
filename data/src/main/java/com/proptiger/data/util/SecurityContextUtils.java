760fa1c8b41bfcf3d1c0fdc098ebae0d62e4a3ce
Rajeev Pandey <rajeev.pandey@proptiger.com>
2014-06-24 19:45:56 +0530

    [#MIDL-19] code formatting

--- data/src/main/java/com/proptiger/data/util/SecurityContextUtils.java
+++ data/src/main/java/com/proptiger/data/util/SecurityContextUtils.java
@@ -6,19 +6,20 @@ import com.proptiger.data.internal.dto.ActiveUser;
 
 /**
  * Security utils to get current logged in user
+ * 
  * @author Rajeev Pandey
  *
  */
 public class SecurityContextUtils {
-    
+
     /**
      * @return ActiveUser object or null if user is not logged in
      */
-    public static ActiveUser getLoggedInUser(){
+    public static ActiveUser getLoggedInUser() {
         ActiveUser activeUser = null;
         Object activeUserObj = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if(activeUserObj instanceof ActiveUser){
-            activeUser = (ActiveUser)activeUserObj;
+        if (activeUserObj instanceof ActiveUser) {
+            activeUser = (ActiveUser) activeUserObj;
         }
         return activeUser;
     }
