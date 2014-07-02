96aedec1fd2caf8831dcfcf7a39e8707d4f62d05
Rajeev Pandey <rajeev.pandey@proptiger.com>
2014-07-02 17:58:32 +0530

    [#MIDL-338] api access log in logfile and db foranonymous and logged in users

---
+++ data/src/main/java/com/proptiger/app/config/AppSchedulingConfig.java
@@ -0,0 +1,16 @@
+package com.proptiger.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * Enable sceduling and could be used to enable dynamic scheduling.
+ * 
+ * @author Rajeev Pandey
+ *
+ */
+@Configuration
+@EnableScheduling
+public class AppSchedulingConfig {
+
+}
