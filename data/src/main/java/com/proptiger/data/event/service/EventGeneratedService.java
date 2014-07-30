74c341b6ae4d39edd9c3fa1e9ae3b58ab9b3810e
Mukand Agarwal <mukand.agarwal@proptiger.com>
2014-07-30 19:29:16 +0530

    fixing imports.

--- data/src/main/java/com/proptiger/data/event/service/EventGeneratedService.java
+++ data/src/main/java/com/proptiger/data/event/service/EventGeneratedService.java
@@ -14,13 +14,11 @@ import org.springframework.transaction.annotation.Transactional;
 import com.proptiger.data.event.enums.DBOperation;
 import com.proptiger.data.event.model.EventGenerated;
 import com.proptiger.data.event.model.EventGenerated.EventStatus;
-import com.proptiger.data.event.model.payload.EventTypePayload;
 import com.proptiger.data.event.model.EventType;
 import com.proptiger.data.event.model.RawDBEvent;
+import com.proptiger.data.event.model.payload.EventTypePayload;
 import com.proptiger.data.event.repo.DBEventMappingDao;
 import com.proptiger.data.event.repo.EventGeneratedDao;
-import com.proptiger.data.event.util.DBEventMapper;
-import com.proptiger.data.model.event.payload.DefaultEventTypePayload;
 import com.proptiger.data.service.LocalityService;
 
 @Service
