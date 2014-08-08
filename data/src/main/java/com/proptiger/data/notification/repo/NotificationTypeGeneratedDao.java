22ff6844a718c899d24d0fa90e96cf9cbd731fd1
Sahil Garg <sahil.garg@proptiger.com>
2014-08-08 16:14:31 +0530

    Adding SubscriberConfigDao

--- data/src/main/java/com/proptiger/data/notification/repo/NotificationTypeGeneratedDao.java
+++ data/src/main/java/com/proptiger/data/notification/repo/NotificationTypeGeneratedDao.java
@@ -3,10 +3,10 @@ package com.proptiger.data.notification.repo;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-import com.proptiger.data.event.model.EventGenerated;
 import com.proptiger.data.notification.enums.NotificationStatus;
+import com.proptiger.data.notification.model.NotificationTypeGenerated;
 
-public interface NotificationTypeGeneratedDao extends PagingAndSortingRepository<EventGenerated, Integer> {
+public interface NotificationTypeGeneratedDao extends PagingAndSortingRepository<NotificationTypeGenerated, Integer> {
 
     @Query("Select count(id) from NotificationTypeGenerated N where N.notificationStatus = ?1 ")
     public Integer getNotificationTypeCountByNotificationStatus(NotificationStatus status);
--- data/src/main/java/com/proptiger/data/notification/service/SubscriberConfigService.java
+++ data/src/main/java/com/proptiger/data/notification/service/SubscriberConfigService.java
@@ -1,11 +1,32 @@
 package com.proptiger.data.notification.service;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.proptiger.data.notification.model.SubscriberConfig;
+import com.proptiger.data.notification.repo.SubscriberConfigDao;
 
 @Service
 public class SubscriberConfigService {
+
+    @Autowired
+    private SubscriberConfigDao        subscriberConfigDao;
+
+    private static Map<String, String> subscriberConfigMap = new HashMap<String, String>();
+
+    @PostConstruct
+    public void constructSubscriberConfig() {
+        Iterable<SubscriberConfig> subscriberConfigList = subscriberConfigDao.findAll();
+        Iterator<SubscriberConfig> subscriberConfigIterator = subscriberConfigList.iterator();
+        
+    }
 
     public Integer getMaxActiveNotificationTypeCount() {
         return null;
@@ -14,8 +35,13 @@ public class SubscriberConfigService {
     public Date getLastEventDateReadByNotification() {
         return null;
     }
-    
+
     public Date setLastEventDateReadByNotification(Date lastEventDate) {
         return null;
     }
+
+    public Map<String, String> getSubscriberConfigMap() {
+        return subscriberConfigMap;
+    }
+
 }
