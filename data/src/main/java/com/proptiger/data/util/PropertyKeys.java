1a944224e90733dd932bd154fa9a1ba594e21e27
Rajeev Pandey <rajeev.pandey@proptiger.com>
2014-08-11 14:32:43 +0530

    [#MIDL-338] api security warning and code refactoring

--- data/src/main/java/com/proptiger/data/util/PropertyKeys.java
+++ data/src/main/java/com/proptiger/data/util/PropertyKeys.java
@@ -54,6 +54,7 @@ public final class PropertyKeys {
     public static final String AVATAR_IMAGE_URL                           = "avatar.image.url";
     public static final String ACCESS_LOG_INTERNAL_DS_SIZE_THRESHOLD      = "assesslog.internal.ds.size.threshold";
     public static final String ILLEGAL_API_ACCESS_THRESHOLD_COUNT         = "illegal.api.access.threshold.count";
-    public static final String ENABLE_BOT_PREVENTAION                     = "enable.bot.prevention";
+    public static final String ENABLE_CRAWL_PREVENTAION                   = "enable.crawl.prevention";
+    public static final String ENABLE_CRAWL_PREVENTAION_WARNING           = "enable.crawl.prevention.warning";
     public static final String MAIL_FROM_SUPPORT                          = "mail.from.support";
 }
