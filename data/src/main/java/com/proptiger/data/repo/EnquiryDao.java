4a6539326a5e2ed57e3df8ce501c7155fbf2279f
Nikhil Singhal <nikhil.singhal@proptiger.com>
2014-08-12 19:59:10 +0530

    [#MIDL - 9] Dao classes changed

--- data/src/main/java/com/proptiger/data/repo/EnquiryDao.java
+++ data/src/main/java/com/proptiger/data/repo/EnquiryDao.java
@@ -17,11 +17,14 @@ import com.proptiger.data.model.Enquiry.EnquiryCustomDetails;
  * @author mukand
  */
 public interface EnquiryDao extends JpaRepository<Enquiry, Serializable> {
+
+    @Query("SELECT E FROM Enquiry E JOIN FETCH E.project as P WHERE P.version = 'Website' AND E.email = ?1")
     public List<Enquiry> findByEmail(String email);
 
     @Query("select NEW com.proptiger.data.model.Enquiry$EnquiryCustomDetails(E.projectName, E.cityName, P.projectUrl, E.createdDate, B.name) " + " from  Enquiry E left join E.project P "
             + " left join P.builder B where P.version='Website' AND E.email=?1 AND E.localityId > 0 order by E.createdDate DESC ")
     public List<EnquiryCustomDetails> findEnquiriesByEmail(String email);
 
+    @Query("SELECT E FROM Enquiry E JOIN FETCH E.project as P WHERE P.version = 'Website' AND E.email = ?1 AND E.projectId = ?2 ORDER BY E.createdDate DESC")
     public List<Enquiry> findEnquiryByEmailAndProjectIdOrderByCreatedDateDesc(String email, Long projectId);
 }
--- data/src/main/java/com/proptiger/data/repo/user/UserWishListDao.java
+++ data/src/main/java/com/proptiger/data/repo/user/UserWishListDao.java
@@ -3,6 +3,7 @@ package com.proptiger.data.repo.user;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.proptiger.data.model.UserWishlist;
 
@@ -12,12 +13,16 @@ import com.proptiger.data.model.UserWishlist;
  */
 public interface UserWishListDao extends JpaRepository<UserWishlist, Integer> {
 
+    @Query("SELECT U FROM UserWishlist U JOIN FETCH U.project as P WHERE P.version = 'Website' AND U.userId= ?1")
     public List<UserWishlist> findByUserId(Integer userId);
-    
+
+    @Query("SELECT U FROM UserWishlist U JOIN FETCH U.project as P WHERE P.version = 'Website' AND U.userId= ?1 AND U.typeId = null")
     public List<UserWishlist> findByUserIdAndTypeIdIsNull(Integer userId);
-    
+
+    @Query("SELECT U FROM UserWishlist U JOIN FETCH U.project as P WHERE P.version = 'Website' AND U.userId= ?1 AND U.typeId != null")
     public List<UserWishlist> findByUserIdAndTypeIdIsNotNull(Integer userId);
-   
+
+    @Query("SELECT U FROM UserWishlist U JOIN FETCH U.project as P WHERE P.version = 'Website' AND U.projectId = ?1 AND  U.userId = ?2")
     public UserWishlist findByProjectIdAndUserId(int projectId, int userId);
 
 }
