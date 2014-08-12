90ac204a2030fac47f3b453d57235f18190ba8f7
Nikhil Singhal <nikhil.singhal@proptiger.com>
2014-08-12 20:36:35 +0530

    solr changes

--- data/src/main/java/com/proptiger/data/repo/user/UserWishListDao.java
+++ data/src/main/java/com/proptiger/data/repo/user/UserWishListDao.java
@@ -16,10 +16,10 @@ public interface UserWishListDao extends JpaRepository<UserWishlist, Integer> {
     @Query("SELECT U FROM UserWishlist U JOIN FETCH U.project as P WHERE P.version = 'Website' AND U.userId= ?1")
     public List<UserWishlist> findByUserId(Integer userId);
 
-    @Query("SELECT U FROM UserWishlist U JOIN FETCH U.project as P WHERE P.version = 'Website' AND U.userId= ?1 AND U.typeId = null")
+    @Query("SELECT U FROM UserWishlist U JOIN FETCH U.project as P WHERE P.version = 'Website' AND U.userId= ?1 AND U.typeId is null")
     public List<UserWishlist> findByUserIdAndTypeIdIsNull(Integer userId);
 
-    @Query("SELECT U FROM UserWishlist U JOIN FETCH U.project as P WHERE P.version = 'Website' AND U.userId= ?1 AND U.typeId != null")
+    @Query("SELECT U FROM UserWishlist U JOIN FETCH U.project as P WHERE P.version = 'Website' AND U.userId= ?1 AND U.typeId is not null")
     public List<UserWishlist> findByUserIdAndTypeIdIsNotNull(Integer userId);
 
     @Query("SELECT U FROM UserWishlist U JOIN FETCH U.project as P WHERE P.version = 'Website' AND U.projectId = ?1 AND  U.userId = ?2")
