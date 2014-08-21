c13191c5049516a617966c851c4b0f6e3f91d28c
anubhav <anubhav.singh@proptiger.com>
2014-08-21 12:43:05 +0530

    changes

---
+++ data/src/main/java/com/proptiger/data/repo/marketplace/LeadOfferDaoImpl.java
@@ -0,0 +1,32 @@
+package com.proptiger.data.repo.marketplace;
+
+import java.util.List;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.proptiger.data.model.filter.AbstractQueryBuilder;
+import com.proptiger.data.model.filter.JPAQueryBuilder;
+import com.proptiger.data.model.marketplace.LeadOffer;
+import com.proptiger.data.pojo.FIQLSelector;
+import com.proptiger.data.pojo.response.PaginatedResponse;
+
+public class LeadOfferDaoImpl {
+
+    @Autowired
+    private EntityManagerFactory emf;
+    
+    public PaginatedResponse<List<LeadOffer>> getLeadOffers(FIQLSelector selector) {
+        if (selector == null) {
+            selector = new FIQLSelector();
+        }
+        AbstractQueryBuilder<LeadOffer> leadOffer = new JPAQueryBuilder<>(emf.createEntityManager(), LeadOffer.class);
+        leadOffer.buildQuery(selector);
+        PaginatedResponse<List<LeadOffer>> paginatedResponse = new PaginatedResponse<>();
+        paginatedResponse.setResults(leadOffer.retrieveResults());
+        paginatedResponse.setTotalCount(leadOffer.retrieveCount());
+        return paginatedResponse;
+    }
+    
+}
