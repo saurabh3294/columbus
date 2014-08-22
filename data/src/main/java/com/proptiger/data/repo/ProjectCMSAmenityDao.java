a202e99ef6dd330e7f6b82b44c6d3be3a9d4ef27
Rajeev Pandey <rajeev.pandey@proptiger.com>
2014-08-22 18:13:49 +0530

    [#RMP-84] listing amenity create change to add unverified amenity

--- data/src/main/java/com/proptiger/data/repo/ProjectCMSAmenityDao.java
+++ data/src/main/java/com/proptiger/data/repo/ProjectCMSAmenityDao.java
@@ -3,12 +3,12 @@ package com.proptiger.data.repo;
 import java.util.List;
 import java.util.Set;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.proptiger.data.model.ProjectCMSAmenity;
 
-public interface ProjectCMSAmenityDao extends PagingAndSortingRepository<ProjectCMSAmenity, Long> {
+public interface ProjectCMSAmenityDao extends JpaRepository<ProjectCMSAmenity, Long> {
     @Query("SELECT pca FROM ProjectCMSAmenity pca JOIN FETCH pca.amenityMaster am WHERE pca.amenityId=am.amenityId AND pca.projectId=?1")
     List<ProjectCMSAmenity> findByProjectId(int projectId);
     
