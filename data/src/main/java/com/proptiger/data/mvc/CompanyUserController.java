f22e5ec3c8de6b306945cd2a5ece8c4e3a11831e
anubhav <anubhav.singh@proptiger.com>
2014-09-02 11:42:31 +0530

    changes

--- data/src/main/java/com/proptiger/data/mvc/CompanyUserController.java
+++ data/src/main/java/com/proptiger/data/mvc/CompanyUserController.java
@@ -3,14 +3,15 @@ package com.proptiger.data.mvc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.proptiger.data.internal.dto.ActiveUser;
 import com.proptiger.data.model.companyuser.CompanyUser;
 import com.proptiger.data.pojo.FIQLSelector;
 import com.proptiger.data.pojo.response.APIResponse;
 import com.proptiger.data.service.companyuser.CompanyUserService;
+import com.proptiger.data.util.Constants;
 
 /**
  * @author Rajeev Pandey
@@ -22,10 +23,10 @@ public class CompanyUserController extends BaseController {
     @Autowired
     private CompanyUserService companyUserService;
 
-    @RequestMapping(value = "data/v1/entity/company-user/{companyUserId}")
+    @RequestMapping(value = "data/v1/entity/user/company-user")
     @ResponseBody
-    public APIResponse get(@PathVariable int companyUserId, @ModelAttribute FIQLSelector selector) {
-        CompanyUser agent = companyUserService.getAgent(companyUserId, selector);
+    public APIResponse get(@ModelAttribute FIQLSelector selector , @ModelAttribute(Constants.LOGIN_INFO_OBJECT_NAME) ActiveUser activeUser) {
+        CompanyUser agent = companyUserService.getAgent(activeUser.getUserIdentifier(), selector);
         return new APIResponse(super.filterFieldsFromSelector(agent, selector));
     }
 }
