0989e5fd3e1faab38dfb8290789f269e7763c686
Mandeep Dhir <mandeep.dhir@proptiger.com>
2014-09-05 15:27:41 +0530

    Fixing bug

--- data/src/main/java/com/proptiger/data/repo/LeadTaskDao.java
+++ data/src/main/java/com/proptiger/data/repo/LeadTaskDao.java
@@ -40,10 +40,10 @@ public interface LeadTaskDao extends JpaRepository<LeadTask, Integer> {
             value = "SELECT LT FROM LeadTask LT INNER JOIN FETCH LT.offeredListingMappings TOLM INNER JOIN FETCH TOLM.offeredListing where LT.id IN (?1)")
     public List<LeadTask> getListingMappedTasksByTaskIds(List<Integer> taskIds);
 
-    @Query("select LT from LeadTask LT JOIN FETCH LT.taskStatus LTS JOIN FETCH LTS.resultingStatus INNER JOIN FETCH LTS.masterLeadTask MLT INNER JOIN FETCH LTS.masterLeadTaskStatus MLTS LEFT JOIN FETCH LT.statusReason WHERE LT.leadOfferId in (?1)")
+    @Query("select LT from LeadTask LT JOIN FETCH LT.taskStatus LTS LEFT JOIN FETCH LTS.resultingStatus INNER JOIN FETCH LTS.masterLeadTask MLT INNER JOIN FETCH LTS.masterLeadTaskStatus MLTS LEFT JOIN FETCH LT.statusReason WHERE LT.leadOfferId in (?1)")
     public List<LeadTask> findTasksByLeadOfferId(int leadOfferId);
 
-    @Query("select LT from LeadTask LT JOIN FETCH LT.taskStatus LTS JOIN FETCH LTS.resultingStatus INNER JOIN FETCH LTS.masterLeadTask MLT INNER JOIN FETCH LTS.masterLeadTaskStatus MLTS LEFT JOIN FETCH LT.statusReason WHERE LT.id in (?1)")
+    @Query("select LT from LeadTask LT JOIN FETCH LT.taskStatus LTS LEFT JOIN FETCH LTS.resultingStatus INNER JOIN FETCH LTS.masterLeadTask MLT INNER JOIN FETCH LTS.masterLeadTaskStatus MLTS LEFT JOIN FETCH LT.statusReason WHERE LT.id in (?1)")
     public List<LeadTask> findById(List<Integer> leadTaskIds);
 
     @Query(
