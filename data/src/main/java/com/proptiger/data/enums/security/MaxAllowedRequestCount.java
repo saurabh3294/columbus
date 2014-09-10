7cc534a28f477d436f695b0a96d675211455f946
Rajeev Pandey <rajeev.pandey@proptiger.com>
2014-09-10 12:15:09 +0530

    [#MIDL-334] request count from property file and whitelisting IP

--- data/src/main/java/com/proptiger/data/enums/security/MaxAllowedRequestCount.java
+++ data/src/main/java/com/proptiger/data/enums/security/MaxAllowedRequestCount.java
@@ -1,33 +1,54 @@
 package com.proptiger.data.enums.security;
 
-public enum MaxAllowedRequestCount {
-    LIMIT_PER_MINUTE(60, 200, 30), LIMIT_PER_DAY(86400, 5000, 150);
+import com.proptiger.data.util.PropertyKeys;
+import com.proptiger.data.util.PropertyReader;
 
-    private String  label;
+/**
+ * Enum that hold value for allowable request count per request method in a time
+ * frame from a user. Values will be picked from application.properties file, if
+ * values not defined in property file then default values will be used as
+ * defined in enum definition.
+ * 
+ * This enum should be used only after context load other wise
+ * default values will be returned.
+ *
+ * @author Rajeev Pandey
+ */
+
+public enum MaxAllowedRequestCount {
+    REQUEST_PER_MIN(60, 200, 5), REQUEST_PER_DAY(86400, 5000, 50);
+
     private Integer timeFrame;
-    private Integer allowedRequestCount;
+    private Integer allowedAllRequestCount;
     private Integer allowedPostRequestCount;
 
     private MaxAllowedRequestCount(Integer timeFrame, Integer allowedRequestCount, Integer allowedPostRequestCount) {
         this.timeFrame = timeFrame;
-        this.allowedRequestCount = allowedRequestCount;
-        this.label = this.name();
+        this.allowedAllRequestCount = allowedRequestCount;
         this.allowedPostRequestCount = allowedPostRequestCount;
     }
 
     public Integer getAllowedPostRequestCount() {
+        Integer val = getValue(PropertyKeys.REQ_POST_COUNT);
+        if (val != null) {
+            return val;
+        }
         return allowedPostRequestCount;
-    }
-
-    public String getLabel() {
-        return label;
     }
 
     public Integer getTimeFrame() {
         return timeFrame;
     }
 
-    public Integer getAllowedRequestCount() {
-        return allowedRequestCount;
+    public Integer getAllowedAllRequestCount() {
+        Integer val = getValue(PropertyKeys.REQ_ALL_COUNT);
+        if (val != null) {
+            return val;
+        }
+        return allowedAllRequestCount;
+    }
+
+    private Integer getValue(String key) {
+        return PropertyReader.getRequiredPropertyAsType(this.name() + "." + key, Integer.class);
     }
 }
