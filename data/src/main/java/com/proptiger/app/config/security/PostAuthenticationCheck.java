f41dc529ba70993ff708fc24a83d5672962064b7
anujkaushik <anuj.kaushik@proptiger.com>
2014-09-22 18:20:35 +0530

    BB-545 : Different status codes for B2B login failures.

--- data/src/main/java/com/proptiger/app/config/security/PostAuthenticationCheck.java
+++ data/src/main/java/com/proptiger/app/config/security/PostAuthenticationCheck.java
@@ -6,13 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsChecker;
 
+import com.proptiger.data.constants.ResponseCodes;
+import com.proptiger.data.constants.ResponseErrorMessages;
 import com.proptiger.data.enums.Application;
 import com.proptiger.data.internal.dto.ActiveUser;
 import com.proptiger.data.service.user.UserSubscriptionService;
 import com.proptiger.exception.AuthenticationExceptionImpl;
 
 /**
- * Class to handle post auth checks. After successfull credential validation
+ * Class to handle post auth checks. After successful credential validation
  * there may be some checks say on type of user and their roles and access type.
  * 
  * @author Rajeev Pandey
@@ -22,9 +24,6 @@ public class PostAuthenticationCheck implements UserDetailsChecker {
 
     @Autowired
     private UserSubscriptionService userSubscriptionService;
-
-    private String                  errorMessageNonB2BUser               = "Invalid userid and password. Please send mail to datalabs@proptiger.com for verifying userid and password.";
-    private String                  errorMessageExpiredPermissionB2BUser = "Thanks for using our product. Validity of your subscription has expired. To continue using this service, please connect with your relationship manager or send us mail at datalabs@proptiger.com";
 
     @Override
     public void check(UserDetails toCheck) {
@@ -50,13 +49,13 @@ public class PostAuthenticationCheck implements UserDetailsChecker {
              */
             List<?> userSubscriptionMappingList = userSubscriptionService.getUserSubscriptionMappingList(userId);
             if (userSubscriptionMappingList == null || userSubscriptionMappingList.isEmpty()) {
-                throw new AuthenticationExceptionImpl(errorMessageNonB2BUser);
+                throw new AuthenticationExceptionImpl(ResponseCodes.ACCESS_DENIED, ResponseErrorMessages.NON_B2B_USER);
             }
 
             /* Throw error if user has no *active* subscriptions. */
             List<?> permissionList = userSubscriptionService.getUserAppSubscriptionDetails(userId);
             if (permissionList == null || permissionList.isEmpty()) {
-                throw new AuthenticationExceptionImpl(errorMessageExpiredPermissionB2BUser);
+                throw new AuthenticationExceptionImpl(ResponseCodes.ACCESS_EXPIRED, ResponseErrorMessages.EXPIRED_PERMISSION_B2B_USER);
             }
         }
 
--- data/src/main/java/com/proptiger/data/util/FilterAuthTrendRequest.java
+++ data/src/main/java/com/proptiger/data/util/FilterAuthTrendRequest.java
@@ -1,5 +1,7 @@
 package com.proptiger.data.util;
 
+import java.util.List;
+
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -7,10 +9,13 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.proptiger.data.constants.ResponseCodes;
+import com.proptiger.data.constants.ResponseErrorMessages;
 import com.proptiger.data.internal.dto.ActiveUser;
 import com.proptiger.data.pojo.FIQLSelector;
 import com.proptiger.data.service.ApplicationNameService;
 import com.proptiger.data.service.user.UserSubscriptionService;
+import com.proptiger.exception.AuthenticationExceptionImpl;
 
 /**
  * This class appends the subscription permissions for logged in user to the
@@ -35,6 +40,15 @@ public class FilterAuthTrendRequest {
         }
         ActiveUser user = SecurityContextUtils.getLoggedInUser();
         if (user != null) {
+            
+            /* If all of users's permissions have expired then log him out */
+            List<?> permissionList = userSubscriptionService.getUserAppSubscriptionDetails(user.getUserIdentifier());
+            if (permissionList == null || permissionList.isEmpty()) {
+                throw new AuthenticationExceptionImpl(
+                        ResponseCodes.ACCESS_EXPIRED,
+                        ResponseErrorMessages.EXPIRED_PERMISSION_B2B_USER);
+            }
+            
             Object[] methodArgs = jointPoint.getArgs();
             for (Object arg : methodArgs) {
                 if (arg != null && arg.getClass().equals(FIQLSelector.class)) {
