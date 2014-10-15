e822203ac0d8b91cb981e5f04b42a1afaee3de9d
AzitabhAjit <azitabh@gmail.com>
2014-10-15 14:57:27 +0530

    initial commits

---
+++ src/main/java/com/proptiger/search/model/Typeahead.java
@@ -0,0 +1,319 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.proptiger.search.model;
+
+import javax.persistence.Transient;
+
+import org.apache.solr.client.solrj.beans.Field;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.proptiger.core.model.BaseModel;
+
+/**
+ * 
+ * @author mukand
+ */
+@JsonAutoDetect(fieldVisibility = Visibility.ANY, getterVisibility = Visibility.NONE, isGetterVisibility = Visibility.NONE)
+@JsonInclude(Include.NON_NULL)
+@JsonFilter("fieldFilter")
+public class Typeahead extends BaseModel {
+
+	private static final long serialVersionUID = 2096261268711516512L;
+
+	@Field(value = "id")
+	private String id;
+
+	@Transient
+	private boolean authorized = false;
+
+	@Field(value = "TYPEAHEAD_LABEL")
+	private String label;
+
+	@Field(value = "TYPEAHEAD_LABEL_NGRAMS")
+	@JsonIgnore
+	private String labelNgrams;
+
+	@Field(value = "TYPEAHEAD_CITY")
+	private String city;
+
+	@Field(value = "tp_locality")
+	private String locality;
+
+	@Field(value = "TYPEAHEAD_REDIRECT_URL")
+	private String redirectUrl;
+
+	@Field(value = "TYPEAHEAD_TYPE")
+	private String type;
+
+	@Field(value = "TYPEAHEAD_DISPLAY_TEXT")
+	private String displayText;
+
+	@Field(value = "TYPEAHEAD_CORE_TEXT")
+	private String coreText;
+
+	@Field(value = "TYPEAHEAD_CORE_TEXT_NGRAMS")
+	private String coreTextNgrams;
+
+	@Field(value = "LOCALITY_URL")
+	private String localityURL;
+
+	@Field(value = "LOCALITY_ID")
+	private Integer localityId;
+
+	@Field(value = "CITY_ID")
+	private Integer cityId;
+
+	@Field(value = "TYPEAHEAD_LABEL_LOWERCASE")
+	private String labelLowercase;
+
+	@Field(value = "TYPEAHEAD_LOCALITY_AVG_PRICE_PER_UNIT_AREA")
+	private Double localityAvgPricePerUnitArea;
+
+	@Field(value = "TYPEAHEAD_LOCALITY_PRICE_APPRECIATION_6MONTHS")
+	private Double localityPriceRise6Months;
+
+	@Field(value = "TYPEAHEAD_LOCALITY_UNITS_LAUNCHED_6MONTHS")
+	private Integer localityUnitsLaunched6Months;
+
+	@Field(value = "TYPEAHEAD_LOCALITY_UNITS_SOLD_6MONTHS")
+	private Integer localityUnitsSold6Months;
+
+	@Field(value = "TYPEAHEAD_LOCALITY_UNITS_DELIVERED_6MONTHS")
+	private Integer localityUnitsDelivered6Months;
+
+	@Field(value = "TEMPLATE_TEXT")
+	private String templateText;
+
+	@Field(value = "TEMPLATE_TYPE")
+	private String templateType;
+
+	@Field(value = "score")
+	private Float score;
+
+	@Field(value = "LATITUDE")
+	private Double latitude;
+
+	@Field(value = "LONGITUDE")
+	private Double longitude;
+
+	@Transient
+	private Boolean isSuggestion = false;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public String getLabelNgrams() {
+		return labelNgrams;
+	}
+
+	public void setLabelNgrams(String labelNgrams) {
+		this.labelNgrams = labelNgrams;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getLocality() {
+		return locality;
+	}
+
+	public void setLocality(String locality) {
+		this.locality = locality;
+	}
+
+	public String getRedirectUrl() {
+		return redirectUrl;
+	}
+
+	public void setRedirectUrl(String redirectUrl) {
+		this.redirectUrl = redirectUrl;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getDisplayText() {
+		return displayText;
+	}
+
+	public void setDisplayText(String displayText) {
+		this.displayText = displayText;
+	}
+
+	public String getCoreText() {
+		return coreText;
+	}
+
+	public void setCoreText(String coreText) {
+		this.coreText = coreText;
+	}
+
+	public String getCoreTextNgrams() {
+		return coreTextNgrams;
+	}
+
+	public void setCoreTextNgrams(String coreTextNgrams) {
+		this.coreTextNgrams = coreTextNgrams;
+	}
+
+	public String getLocalityURL() {
+		return localityURL;
+	}
+
+	public void setLocalityURL(String localityURL) {
+		this.localityURL = localityURL;
+	}
+
+	public String getLabelLowercase() {
+		return labelLowercase;
+	}
+
+	public void setLabelLowercase(String labelLowercase) {
+		this.labelLowercase = labelLowercase;
+	}
+
+	public Double getLocalityAvgPricePerUnitArea() {
+		return localityAvgPricePerUnitArea;
+	}
+
+	public void setLocalityAvgPricePerUnitArea(
+			Double localityAvgPricePerUnitArea) {
+		this.localityAvgPricePerUnitArea = localityAvgPricePerUnitArea;
+	}
+
+	public Double getLocalityPriceRise6Months() {
+		return localityPriceRise6Months;
+	}
+
+	public void setLocalityPriceRise6Months(Double localityPriceRise6Months) {
+		this.localityPriceRise6Months = localityPriceRise6Months;
+	}
+
+	public Integer getLocalityUnitsLaunched6Months() {
+		return localityUnitsLaunched6Months;
+	}
+
+	public void setLocalityUnitsLaunched6Months(
+			Integer localityUnitsLaunched6Months) {
+		this.localityUnitsLaunched6Months = localityUnitsLaunched6Months;
+	}
+
+	public Integer getLocalityUnitsSold6Months() {
+		return localityUnitsSold6Months;
+	}
+
+	public void setLocalityUnitsSold6Months(Integer localityUnitsSold6Months) {
+		this.localityUnitsSold6Months = localityUnitsSold6Months;
+	}
+
+	public Integer getLocalityUnitsDelivered6Months() {
+		return localityUnitsDelivered6Months;
+	}
+
+	public void setLocalityUnitsDelivered6Months(
+			Integer localityUnitsDelivered6Months) {
+		this.localityUnitsDelivered6Months = localityUnitsDelivered6Months;
+	}
+
+	public boolean isAuthorized() {
+		return authorized;
+	}
+
+	public void setAuthorized(boolean authorized) {
+		this.authorized = authorized;
+	}
+
+	public Integer getLocalityId() {
+		return localityId;
+	}
+
+	public void setLocalityId(Integer localityId) {
+		this.localityId = localityId;
+	}
+
+	public Integer getCityId() {
+		return cityId;
+	}
+
+	public void setCityId(Integer cityId) {
+		this.cityId = cityId;
+	}
+
+	public Float getScore() {
+		return score;
+	}
+
+	public void setScore(Float score) {
+		this.score = score;
+	}
+
+	public String getTemplateText() {
+		return templateText;
+	}
+
+	public void setTemplateText(String templateText) {
+		this.templateText = templateText;
+	}
+
+	public String getTemplateType() {
+		return templateType;
+	}
+
+	public void setTemplateType(String templateType) {
+		this.templateType = templateType;
+	}
+
+	public Double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+
+	public Double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+
+	public Boolean getIsSuggestion() {
+		return isSuggestion;
+	}
+
+	public void setIsSuggestion(Boolean isSuggestion) {
+		this.isSuggestion = isSuggestion;
+	}
+}
---
+++ src/main/java/com/proptiger/search/repo/TypeaheadDao.java
@@ -0,0 +1,334 @@
+package com.proptiger.search.repo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
+import com.proptiger.core.repo.SolrDao;
+import com.proptiger.core.util.UtilityClass;
+import com.proptiger.search.model.Typeahead;
+
+/**
+ * 
+ * @author mukand
+ * @author hemendra
+ * 
+ */
+
+@Repository
+public class TypeaheadDao {
+	@Autowired
+	private SolrDao solrDao;
+
+	@Value("${test}")
+	private String coreApiUrl;
+
+	private float boostStart = 10f;
+	private float boostMultiplier = 0.3f;
+
+	public QueryResponse getResponseSuggestions(String query, int rows,
+			List<String> filterQueries) {
+		SolrQuery solrQuery = getSimpleSolrQuery(query, rows, filterQueries);
+		QueryResponse result = solrDao.executeQuery(solrQuery);
+		return result;
+	}
+
+	public QueryResponse getResponseV2(String query, int rows,
+			List<String> filterQueries) {
+		SolrQuery solrQuery = getSolrQueryV2(query, rows, filterQueries);
+		QueryResponse result = solrDao.executeQuery(solrQuery);
+		return result;
+	}
+
+	public List<Typeahead> getTypeaheadsV2(String query, int rows,
+			List<String> filterQueries) {
+
+		// List<String> filterQueriesOld = new ArrayList<String>(filterQueries);
+		//
+		// // Add the city filter if it exist in the query
+		// List<String> cityList = this.findCities(query);
+		//
+		// // removes city name if query contains other terms too
+		// String new_query = this.parseCities(query, cityList);
+		// for (String city : cityList) {
+		// filterQueries.add("TYPEAHEAD_CITY:" + city);
+		// }
+		//
+		// List<SolrQuery> solrQueries = new ArrayList<SolrQuery>();
+		// solrQueries.add(this.getSolrQueryV2(new_query, rows, filterQueries));
+		// if (!filterQueries.isEmpty()) { // Adding another query if filters
+		// exist
+		// solrQueries.add(this.getSolrQueryV2(query, rows, filterQueriesOld));
+		// }
+
+		SolrQuery solrQuery = this.getSolrQueryV2(query, rows, filterQueries);
+		List<Typeahead> results = getSpellCheckedResponseV2(solrQuery, rows,
+				filterQueries);
+		return UtilityClass.getFirstNElementsOfList(results, rows);
+	}
+
+	// Add parameters to use the custom requestHandler
+	private SolrQuery getSolrQueryV2(String query, int rows,
+			List<String> filterQueries) {
+
+		SolrQuery solrQuery = getSimpleSolrQuery(query, rows, filterQueries);
+		solrQuery.setParam("qt", "/payload");
+		solrQuery.setParam("defType", "payload");
+		solrQuery.setParam("fl", "*,score");
+
+		/* Query time boosting */
+		String boostQuery = getBoostQuery(query);
+		if (!boostQuery.isEmpty()) {
+			solrQuery.setParam("bq", boostQuery);
+		}
+		return solrQuery;
+	}
+
+	private String getBoostQuery(String query) {
+		String boostQuery = "";
+		StringTokenizer st = new StringTokenizer(query.trim());
+		int count = st.countTokens();
+		if (count < 2) {
+			return boostQuery;
+		}
+
+		float boost = boostStart;
+		/* Boost all-but-last query strings as core-texts */
+		for (int i = 0; i < count - 1; i++) {
+			boostQuery += "Core_text:" + st.nextToken() + "^"
+					+ Math.max(1, boost) + " ";
+			boost *= boostMultiplier;
+		}
+
+		/* Boost last query string as an edgeNGram */
+		boostQuery += ("ENGram:" + st.nextToken() + "^" + Math.max(1, boost) + " ");
+
+		// System.out.println("=====>> Q = [" + query + "], BQ = [" + boostQuery
+		// + "]");
+		return boostQuery;
+	}
+
+	private SolrQuery getSimpleSolrQuery(String query, int rows,
+			List<String> filterQueries) {
+		SolrQuery solrQuery = new SolrQuery();
+		solrQuery.setQuery(query);
+		for (String fq : filterQueries) {
+			solrQuery.addFilterQuery(fq);
+		}
+
+		solrQuery.setRows(rows);
+		return solrQuery;
+	}
+
+	/**
+	 * If the query has a typo and can be corrected then new query is generated
+	 * using the suggestions and executed automatically
+	 */
+	private List<Typeahead> getSpellCheckedResponseV2(SolrQuery solrQuery,
+			int rows, List<String> filterQueries) {
+
+		List<Typeahead> results = new ArrayList<Typeahead>();
+		QueryResponse response = solrDao.executeQuery(solrQuery);
+		String spellsuggestion = response.getSpellCheckResponse()
+				.getCollatedResult();
+		results = response.getBeans(Typeahead.class);
+
+		if (spellsuggestion != null && results.size() < 5) {
+			SolrQuery newQuery = this.getSolrQueryV2(
+					spellsuggestion.toString(), rows, filterQueries);
+			List<Typeahead> suggestResults = new ArrayList<Typeahead>();
+			suggestResults = solrDao.executeQuery(newQuery).getBeans(
+					Typeahead.class);
+			results.addAll(suggestResults);
+			return results;
+		} else {
+			return results;
+		}
+	}
+
+	// Previous functions:
+
+	// private SolrQuery getSolrQuery(String query, int rows, List<String>
+	// filterQueries) {
+	// SolrQuery solrQuery = this.getQueryParams(query);
+	//
+	// for (String fq : filterQueries) {
+	// solrQuery.addFilterQuery(fq);
+	// }
+	//
+	// solrQuery.setRows(rows);
+	// return solrQuery;
+	// }
+
+	// private SolrQuery getQueryParams(String query) {
+	// SolrQuery solrQuery = new SolrQuery();
+	// List<String> queryTokens = this.tokenizeQuery(query);
+	// List<String> cityList = this.findCities(query);
+	// query = this.parseCities(query, cityList);
+	// String ctq = this.getCityQuery(cityList);
+	//
+	// double wt = 1.0;
+	// if (query.trim().isEmpty())
+	// wt = 1.0 / queryTokens.size();
+	//
+	// solrQuery.setQuery(query);
+	// solrQuery.setParam("defType", "edismax");
+	// solrQuery.setParam("qf", "tp_engram^5.0 tp_ngram^0.5 tp_phonetic^0.2");
+	//
+	// List<String> boostList = new ArrayList<String>();
+	// for (String qry : queryTokens) {
+	// if (qry.split(" ").length == 1) {
+	// boostList
+	// .add("query({!edismax qf='tp_city tp_locality tp_builder tp_suburb tp_project' tie=0.1 v='$q' boost=''}, "
+	// + String
+	// .format("%.2f", wt) + ")");
+	// }
+	// else {
+	// boostList
+	// .add("query({!edismax qf='tp_city tp_locality tp_builder tp_suburb tp_project' tie=0.1 v='\"$q\"' boost=''}, $wt)");
+	// }
+	// }
+	//
+	// String boost = new String();
+	// if (boostList.size() > 1) {
+	// boost = "sum(" + Joiner.on(",").skipNulls().join(boostList) + ")";
+	// }
+	// else {
+	// boost = boostList.get(0);
+	// }
+	// boost =
+	// "product(map(query({!v='TYPEAHEAD_TYPE:CITY'}),0,0,1,1.5),map(query({!v='TYPEAHEAD_TYPE:BUILDER'}),0,0,1,1.1),map(query({!v='TYPEAHEAD_TYPE:PROJECT'}),0,0,1,1.3),map(query({!v='TYPEAHEAD_TYPE:LOCALITY'}),0,0,1,1.3),map(query({!v='TYPEAHEAD_TYPE:SUBURB'}),0,0,1,1.3),"
+	// + boost
+	// + ")";
+	//
+	// if (cityList.size() > 0) {
+	// boost = "product(query({!v='" + ctq + "'}, 1.0), " + boost + ")";
+	// }
+	//
+	// solrQuery.setParam("boost", boost);
+	// return solrQuery;
+	// }
+
+	// private String getCityQuery(List<String> cityList) {
+	// List<String> newCityList = new ArrayList<String>();
+	//
+	// for (String city : cityList) {
+	// newCityList.add("TYPEAHEAD_CITY:" + city.trim());
+	// }
+	// String ctq = Joiner.on(" OR ").skipNulls().join(newCityList);
+	// return ctq;
+	// }
+
+	public List<List<String>> powerset(List<String> list) {
+		List<List<String>> ps = new ArrayList<List<String>>();
+		ps.add(new ArrayList<String>());
+
+		for (String item : list) {
+			List<List<String>> newPs = new ArrayList<List<String>>();
+
+			for (List<String> subset : ps) {
+				newPs.add(subset);
+				List<String> newSubset = new ArrayList<String>(subset);
+				newSubset.add(item);
+				newPs.add(newSubset);
+			}
+
+			ps = newPs;
+		}
+		return ps;
+	}
+
+	// private List<String> tokenizeQuery(String query) {
+	// List<String> qList = new ArrayList<String>();
+	// List<List<String>> powerset = this.powerset(qList);
+	// for (List<String> list : powerset) {
+	// String st = Joiner.on(" ").skipNulls().join(list);
+	// if (st.trim() != "" && st != null) {
+	// qList.add(st);
+	// }
+	// }
+	// return qList;
+	// }
+
+	// public List<Typeahead> getTypeaheads(String query, int rows, List<String>
+	// filterQueries) {
+	//
+	// SolrQuery solrQuery = this.getSolrQuery(query, rows, filterQueries);
+	// return solrDao.executeQuery(solrQuery).getBeans(Typeahead.class);
+	// }
+
+	// private String parseCities(String query, List<String> queryCities) {
+	// String query_new = this.substituteQuery(query, queryCities);
+	//
+	// if (query_new.trim().isEmpty()) {
+	// return query;
+	// }
+	// else {
+	// return query_new;
+	// }
+	// }
+
+	// private List<String> findCities(String query) {
+	// // TODO Auto-generated method stub
+	// List<City> cityList = cityService.getCityList(null);
+	// List<String> cityLabels = new ArrayList<String>();
+	//
+	// for (City city : cityList) {
+	// String label = city.getLabel();
+	// if (label != "") {
+	// cityLabels.add(city.getLabel());
+	// }
+	// }
+	//
+	// List<String> matchedCities = new ArrayList<String>();
+	// for (String city : cityLabels) {
+	// if (query.matches("(?i).*" + city + ".*")) {
+	// if (city != "") {
+	// matchedCities.add(city);
+	// }
+	// }
+	// }
+	// return matchedCities;
+	// }
+
+	// private String substituteQuery(String query, List<String> terms) {
+	// // replaces terms in query with terms
+	// for (String term : terms) {
+	// query = query.replace(term.toLowerCase(), "");
+	// }
+	// return query.trim();
+	// }
+
+	public List<Typeahead> getExactTypeaheads(String query, int rows,
+			List<String> filterQueries) {
+		String[] multiWords = query.split("\\s+");
+		int wordsCounter = 0;
+		StringBuilder queryStringBuilder = new StringBuilder();
+		for (String word : multiWords) {
+			if (++wordsCounter < multiWords.length) {
+				queryStringBuilder.append("TYPEAHEAD_LABEL_LOWERCASE:" + "*"
+						+ word + "*" + " AND ");
+			} else {
+				queryStringBuilder.append("TYPEAHEAD_LABEL_LOWERCASE:" + "*"
+						+ word + "*");
+			}
+		}
+
+		String exactMatchQuery = queryStringBuilder.toString();
+
+		SolrQuery solrQuery = new SolrQuery();
+		solrQuery.setQuery(exactMatchQuery);
+		for (String fq : filterQueries) {
+			solrQuery.addFilterQuery(fq);
+		}
+		solrQuery.setRows(rows);
+		return solrDao.executeQuery(solrQuery).getBeans(Typeahead.class);
+	}
+
+}
