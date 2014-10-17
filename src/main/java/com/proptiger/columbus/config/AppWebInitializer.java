6aec7b18d9b9a7b68e623af2b3d6fdbe3129b7ec
AzitabhAjit <azitabh@gmail.com>
2014-10-17 13:30:23 +0530

    refatoring webinitializer

---
+++ src/main/java/com/proptiger/columbus/config/AppWebInitializer.java
@@ -0,0 +1,27 @@
+package com.proptiger.columbus.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.springframework.web.WebApplicationInitializer;
+
+import com.proptiger.core.config.WebInitializer;
+import com.proptiger.core.config.WebMvcConfig;
+
+/**
+ * Initialize web application, configuring front controller DispatcherServlet,
+ * this file is replacement of web.xml
+ * 
+ * @author Rajeev Pandey
+ * @author azi
+ * 
+ */
+public class AppWebInitializer extends WebInitializer implements
+		WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext)
+			throws ServletException {
+		super.onServletStartup(servletContext, WebMvcConfig.class);
+	}
+}
