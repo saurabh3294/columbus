a453f61a4f02eff9ed536018bacf3beddeb348cf
anubhav-proptiger <anubhav.singh@proptiger.com>
2014-10-20 13:38:45 +0530

    verified

--- data/src/main/java/com/proptiger/data/repo/ProjectCMSAmenityDao.java
+++ data/src/main/java/com/proptiger/data/repo/ProjectCMSAmenityDao.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import com.proptiger.data.model.ProjectCMSAmenity;
 
 public interface ProjectCMSAmenityDao extends JpaRepository<ProjectCMSAmenity, Long> {
-    @Query("SELECT pca FROM ProjectCMSAmenity pca JOIN FETCH pca.amenityMaster am WHERE pca.amenityId=am.amenityId AND pca.projectId=?1")
+    @Query("SELECT pca FROM ProjectCMSAmenity pca JOIN FETCH pca.amenityMaster am WHERE pca.amenityId=am.amenityId AND pca.projectId=?1 AND pca.verified = 1")
     List<ProjectCMSAmenity> findByProjectId(int projectId);
     
     @Query("SELECT pca FROM ProjectCMSAmenity pca JOIN FETCH pca.amenityMaster am WHERE pca.amenityId=am.amenityId AND pca.projectId=?1 and pca.amenityId IN ?2")
