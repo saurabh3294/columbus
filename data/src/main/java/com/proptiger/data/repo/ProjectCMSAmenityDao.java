c9e1ddb5e26f30da01363bb8b4a2fe69836a2c24
anubhav-proptiger <anubhav.singh@proptiger.com>
2014-10-20 16:32:02 +0530

    name change

--- data/src/main/java/com/proptiger/data/repo/ProjectCMSAmenityDao.java
+++ data/src/main/java/com/proptiger/data/repo/ProjectCMSAmenityDao.java
@@ -10,7 +10,7 @@ import com.proptiger.data.model.ProjectCMSAmenity;
 
 public interface ProjectCMSAmenityDao extends JpaRepository<ProjectCMSAmenity, Long> {
     @Query("SELECT pca FROM ProjectCMSAmenity pca JOIN FETCH pca.amenityMaster am WHERE pca.amenityId=am.amenityId AND pca.projectId=?1 AND pca.verified = 1")
-    List<ProjectCMSAmenity> findByProjectId(int projectId);
+    List<ProjectCMSAmenity> findByProjectIdVerifiedOnly(int projectId);
     
     @Query("SELECT pca FROM ProjectCMSAmenity pca JOIN FETCH pca.amenityMaster am WHERE pca.amenityId=am.amenityId AND pca.projectId=?1 and pca.amenityId IN ?2")
     List<ProjectCMSAmenity> findByProjectIdAndMasterAmenityIds(int projectId, Set<Integer> masterAmenityIds);
