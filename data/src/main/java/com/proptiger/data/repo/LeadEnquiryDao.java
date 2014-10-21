5bcc0ac9bf7c6abd01dbd02471341b9b45119712
Nikhil Singhal <nikhil.singhal@proptiger.com>
2014-10-21 19:01:22 +0530

    lead

--- data/src/main/java/com/proptiger/data/repo/LeadEnquiryDao.java
+++
@@ -1,9 +0,0 @@
-package com.proptiger.data.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.proptiger.data.model.Enquiry;
-
-public interface LeadEnquiryDao extends JpaRepository<Enquiry, Integer> {
-
-}
