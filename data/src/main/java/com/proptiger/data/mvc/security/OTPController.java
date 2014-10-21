65d065536a46828f4472b2efcff9431d5abed78e
Mandeep Dhir <mandeep.dhir@proptiger.com>
2014-10-21 19:16:37 +0530

    merging develop

--- data/src/main/java/com/proptiger/data/mvc/security/OTPController.java
+++ data/src/main/java/com/proptiger/data/mvc/security/OTPController.java
@@ -30,7 +30,7 @@ public class OTPController extends BaseController{
 
     @RequestMapping(value = "app/v1/otp/validate", method = RequestMethod.POST)
     public void validateOTP(
-            @RequestBody Object otp,
+            @RequestBody String otp,
             @ModelAttribute(Constants.LOGIN_INFO_OBJECT_NAME) ActiveUser activeUser,
             HttpServletRequest request,
             HttpServletResponse response) {
