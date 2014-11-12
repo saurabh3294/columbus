8b238a5f23e87944ab3f7f0cc9e10fd303795597
Sahil Garg <sahil.garg@proptiger.com>
2014-11-12 20:20:43 +0530

    Adding custom deserializer

--- data/src/main/java/com/proptiger/data/internal/dto/mail/MediumDetails.java
+++ data/src/main/java/com/proptiger/data/internal/dto/mail/MediumDetails.java
@@ -1,14 +1,11 @@
 package com.proptiger.data.internal.dto.mail;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.proptiger.core.model.BaseModel;
 import com.proptiger.data.notification.enums.MediumType;
+import com.proptiger.data.notification.util.MediumDetailsSerializerDeserializer;
 
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
-@JsonSubTypes({
-        @JsonSubTypes.Type(value = DefaultMediumDetails.class, name = "DefaultMediumDetails"),
-        @JsonSubTypes.Type(value = MailDetails.class, name = "MailDetails") })
+@JsonDeserialize(using = MediumDetailsSerializerDeserializer.class)
 public abstract class MediumDetails extends BaseModel {
 
     private static final long serialVersionUID = 8471921365095921737L;
