8235aa58464541c7d8099a5c820e4dfad7a55f45
anujkaushik <anuj.kaushik@proptiger.com>
2014-12-02 17:35:42 +0530

    Trend Report : Download Limit

--- data/src/main/java/com/proptiger/data/repo/UserSubscriptionMappingDao.java
+++ data/src/main/java/com/proptiger/data/repo/UserSubscriptionMappingDao.java
@@ -13,10 +13,5 @@ public interface UserSubscriptionMappingDao extends JpaRepository<UserSubscripti
    public List<UserSubscriptionMapping> findAllByUserId(int userId);
    
    public UserSubscriptionMapping findById(int id);
-
-   //@Query("select CU from CompanyUser CU join fetch CU.companyCoverages CC join fetch CC.locality L where CU.userId = ?1")
-
-   //@Query("SELECT CS from UserSubscriptionMappings USM join fetch USM.subscription CS where CS.userId = ?1")
-   //public List<CompanySubscription> getCompanySubscriptionsByUserId();
    
 }
