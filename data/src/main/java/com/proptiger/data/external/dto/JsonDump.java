15e639bc1935aa2575a00e212bd7e2896c1aa360
anubhav-proptiger <anubhav.singh@proptiger.com>
2014-12-29 13:58:03 +0530

    json object

---
+++ data/src/main/java/com/proptiger/data/external/dto/JsonDump.java
@@ -0,0 +1,19 @@
+package com.proptiger.data.external.dto;
+
+public class JsonDump {    
+    private String comment;
+    private String tower;
+    
+    public String getTower() {
+        return tower;
+    }
+    public void setTower(String tower) {
+        this.tower = tower;
+    }
+    public String getComment() {
+        return comment;
+    }
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
