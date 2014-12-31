b6b1c79a973691e7f786c57e9f1456d8a7eee357
Nikhil Singhal <nikhil.singhal@proptiger.com>
2014-12-31 13:40:13 +0530

    tracker cookies

--- data/src/main/java/com/proptiger/data/service/CookiesService.java
+++ data/src/main/java/com/proptiger/data/service/CookiesService.java
@@ -25,8 +25,11 @@ public class CookiesService {
         cookieExpiryPeriod = PropertyReader.getRequiredPropertyAsType(PropertyKeys.COOKIE_EXPIRY_PERIOD, Integer.class);
     }
 
-    // /every cookie should be set to empty if not present
-    public void setCookies(HttpServletRequest request, HttpServletResponse response) {
+    public Map<String, String> setCookies(HttpServletRequest request, HttpServletResponse response) {
+
+        // In addition to response object, cookies are added to this map to be
+        // consumed by Enquiry
+        Map<String, String> cookiesMap = new HashMap<String, String>();
 
         String landingPage = null;
         String utmz = null;
@@ -55,6 +58,10 @@ public class CookiesService {
                 }
                 else if (c.getName().equals(CookieConstants.UTMZ)) {
                     utmz = c.getValue();
+                    cookiesMap.put(CookieConstants.UTMZ, utmz);
+                }
+                else if (c.getName().equals(CookieConstants.UTMA)) {
+                    cookiesMap.put(CookieConstants.UTMA, c.getValue());
                 }
             }
         }
@@ -67,25 +74,32 @@ public class CookiesService {
             // setting USER_NETWORK cookie
             network = request.getParameter(CookieConstants.NETWORK);
             if (network != null) {
-                setCookie(CookieConstants.USER_NETWORK, network.toLowerCase(), response);
+                setCookie(CookieConstants.USER_NETWORK, network.toLowerCase(), response, cookiesMap);
             }
 
             // Setting utm cookies
-            setUTMCookies(utmz, request, response);
+            setUTMCookies(utmz, request, response, cookiesMap);
 
             // setting REF_URL cookie
             httpReferer = request.getHeader(CookieConstants.REFERER);
             if (httpReferer != null && !httpReferer.isEmpty()) {
-                setCookie(CookieConstants.REF_URL, httpReferer, response);
+                setCookie(CookieConstants.REF_URL, httpReferer, response, cookiesMap);
             }
 
-            setUserIp(request, response);
-            setLandingPage(request, landingPage, response);
+            setUserIp(request, response, cookiesMap);
+            setLandingPage(request, landingPage, response, cookiesMap);
 
-            // ppc set to true for Paid Traffic
+            // ppc set to true if utm_medium is 'ppc' or 'cpc'
+            if (cookiesMap.get(CookieConstants.USER_MEDIUM) != null && (cookiesMap.get(CookieConstants.USER_MEDIUM) != CookieConstants.CPC || cookiesMap
+                    .get(CookieConstants.USER_MEDIUM) != CookieConstants.PPC)) {
+                ppc = true;
+            }
+
+            // ppc set to true if utm_source is 'adwords' or 'adword'
             if (utmSourceFromRequest.equalsIgnoreCase("adwords") || utmSourceFromRequest.equalsIgnoreCase("adword")) {
                 ppc = true;
             }
+
             // ppc evaluated for Non-Paid Traffic
             else {
                 if (httpReferer != null && httpReferer.contains(CookieConstants.UTM_GCLID)) {
@@ -99,77 +113,104 @@ public class CookiesService {
 
         // Override ppc cookie of user only if it was not true
         if (!originalPPC) {
-            setCookie(CookieConstants.USER_FROM_PPC, String.valueOf(ppc).toUpperCase(), response);
+            setCookie(CookieConstants.USER_FROM_PPC, String.valueOf(ppc).toUpperCase(), response, cookiesMap);
         }
+        return cookiesMap;
     }
 
-    private void setUserIp(HttpServletRequest request, HttpServletResponse response) {
+    private void setUserIp(HttpServletRequest request, HttpServletResponse response, Map<String, String> cookiesMap) {
         String userIP = null;
         if (IPUtils.getClientIP(request) != null) {
             userIP = IPUtils.getClientIP(request);
         }
-        setCookie(CookieConstants.USER_IP, userIP, response);
+        setCookie(CookieConstants.USER_IP, userIP, response, cookiesMap);
     }
 
-    private void setLandingPage(HttpServletRequest request, String landingPage, HttpServletResponse response) {
+    private void setLandingPage(
+            HttpServletRequest request,
+            String landingPage,
+            HttpServletResponse response,
+            Map<String, String> cookiesMap) {
         StringBuffer requestURL = request.getRequestURL();
         String queryString = request.getQueryString();
 
         if (queryString != null) {
             requestURL.append('?').append(queryString).toString();
         }
-        setCookie(CookieConstants.LANDING_PAGE, requestURL.toString(), response);
+        setCookie(CookieConstants.LANDING_PAGE, requestURL.toString(), response, cookiesMap);
     }
 
-    // First extract utm cookies from utmz cookie, if not present, extract them
-    // from request parameters
-    private void setUTMCookies(String utmz, HttpServletRequest request, HttpServletResponse response) {
+    // First extract utm cookies from request parameters, if not present,
+    // extract them from utmz cookie
+    private void setUTMCookies(
+            String utmz,
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Map<String, String> cookiesMap) {
 
         String utmAdgroup = null;
         String utmKeyword = null;
         String utmCampaign = null;
         String utmMedium = null;
+        String utmSource = null;
+        String utmGclid = null;
 
         Map<String, String> utmzCookieMap = new HashMap<String, String>();
 
+        // get utm parameters from request parameters
+        utmAdgroup = request.getParameter(CookieConstants.UTM_ADGROUP);
+        utmCampaign = request.getParameter(CookieConstants.UTM_CAMPAIGN);
+        utmKeyword = request.getParameter(CookieConstants.UTM_TERM);
+        utmMedium = request.getParameter(CookieConstants.UTM_MEDIUM);
+        utmSource = request.getParameter(CookieConstants.UTM_SOURCE);
+        utmGclid = request.getParameter(CookieConstants.UTM_GCLID);
+
+        // Fallback of utmAdgroup, utmCampaign, utmKeyword, utmMedium on
+        // utmz cookie
         if (utmz != null) {
-            // get utm parameters from utmz cookie
+
             getUTMZCookieParams(utmz, utmzCookieMap);
 
-            utmAdgroup = utmzCookieMap.get(CookieConstants.UTM_CSR);
-            utmCampaign = utmzCookieMap.get(CookieConstants.UTM_CCN);
-            utmKeyword = utmzCookieMap.get(CookieConstants.UTM_CTR);
-            utmMedium = utmzCookieMap.get(CookieConstants.UTM_CMD);
-        }
-        // Fallback of utmAdgroup, utmCampaign, utmKeyword, utmMedium on
-        // request parameters
-        if (utmAdgroup == null) {
-            utmAdgroup = request.getParameter(CookieConstants.UTM_ADGROUP);
-        }
-
-        if (utmCampaign == null) {
-            utmCampaign = request.getParameter(CookieConstants.UTM_CAMPAIGN);
-        }
-
-        if (utmKeyword == null) {
-            utmKeyword = request.getParameter(CookieConstants.UTM_TERM);
-        }
-
-        if (utmMedium == null) {
-            utmMedium = request.getParameter(CookieConstants.UTM_MEDIUM);
+            if (utmSource == null) {
+                utmSource = utmzCookieMap.get(CookieConstants.UTM_CSR);
+            }
+            if (utmAdgroup == null) {
+                utmAdgroup = utmzCookieMap.get(CookieConstants.UTM_CCN);
+            }
+            if (utmCampaign == null) {
+                utmCampaign = utmzCookieMap.get(CookieConstants.UTM_CCN);
+            }
+            if (utmKeyword == null) {
+                utmKeyword = utmzCookieMap.get(CookieConstants.UTM_CTR);
+            }
+            if (utmMedium == null) {
+                utmMedium = utmzCookieMap.get(CookieConstants.UTM_CMD);
+            }
+            if (utmGclid == null) {
+                utmGclid = utmzCookieMap.get(CookieConstants.UTM_GCLID);
+            }
         }
 
+        // Overriding utmsource and utmmedium if utmgclid present
+        if (utmGclid != null) {
+            utmSource = "google";
+            utmMedium = "cpc";
+        }
+
+        if (utmSource != null) {
+            setCookie(CookieConstants.USER_FROM, utmSource, response, cookiesMap);
+        }
         if (utmAdgroup != null) {
-            setCookie(CookieConstants.USER_ADGROUP, utmAdgroup, response);
+            setCookie(CookieConstants.USER_ADGROUP, utmAdgroup, response, cookiesMap);
         }
-        if (utmAdgroup != null) {
-            setCookie(CookieConstants.USER_CAMPAIGN, utmCampaign, response);
+        if (utmCampaign != null) {
+            setCookie(CookieConstants.USER_CAMPAIGN, utmCampaign, response, cookiesMap);
         }
         if (utmKeyword != null) {
-            setCookie(CookieConstants.USER_KEYWORD, utmKeyword.toLowerCase(), response);
+            setCookie(CookieConstants.USER_KEYWORD, utmKeyword.toLowerCase(), response, cookiesMap);
         }
         if (utmMedium != null) {
-            setCookie(CookieConstants.USER_MEDIUM, utmMedium.toLowerCase(), response);
+            setCookie(CookieConstants.USER_MEDIUM, utmMedium.toLowerCase(), response, cookiesMap);
         }
     }
 
@@ -193,14 +234,18 @@ public class CookiesService {
             utmzCookieMap.put(CookieConstants.UTM_CSR, utmParams.get(CookieConstants.UTM_CSR));
             utmzCookieMap.put(CookieConstants.UTM_GCLID, utmParams.get(CookieConstants.UTM_GCLID));
         }
-
         return utmzCookieMap;
     }
 
-    public void setCookie(String cookieName, String cookieValue, HttpServletResponse response) {
+    public void setCookie(
+            String cookieName,
+            String cookieValue,
+            HttpServletResponse response,
+            Map<String, String> cookiesMap) {
         Cookie landingPageCookie = new Cookie(cookieName, cookieValue);
         landingPageCookie.setMaxAge(cookieExpiryPeriod);
         landingPageCookie.setPath("/");
         response.addCookie(landingPageCookie);
+        cookiesMap.put(cookieName, cookieValue);
     }
 }
