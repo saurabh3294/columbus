6b029c46e4aaad271cfed44cbe00672636ee2e5d
ahsan <ahsan.khan@proptiger.com>
2015-01-07 18:26:24 +0530

    [#MIDL] Locality landmark images issue fix hotfix

--- data/src/main/java/com/proptiger/data/service/ImageEnricher.java
+++ data/src/main/java/com/proptiger/data/service/ImageEnricher.java
@@ -395,9 +395,6 @@ public class ImageEnricher {
         }
         
         List<LandMark> amenities = localityAmenityService.getLocalityAmenities(locality.getLocalityId(), null);
-        if (amenities == null || amenities.isEmpty()) {
-            return;
-        }
         List<Long> amenityIds = localityAmenityService.getIdListFromAmenities(amenities);
         List<Image> images = imageService.getLandMarkImages(DomainObject.landmark, amenityIds);
         if (images == null || images.isEmpty()) {
