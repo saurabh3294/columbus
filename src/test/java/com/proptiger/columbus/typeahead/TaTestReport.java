c68d0fc788134082bdb67b0a2427629b26c97cb6
anujkaushik <anuj.kaushik@proptiger.com>
2015-03-05 19:07:21 +0530

    showing 'better-results' if a test case fails, in normal test mode.

--- src/test/java/com/proptiger/columbus/typeahead/TaTestReport.java
+++ src/test/java/com/proptiger/columbus/typeahead/TaTestReport.java
@@ -51,6 +51,7 @@ public class TaTestReport {
         taTestReport.position = pos;
         taTestReport.betterResults = getResultsAbovePos(ttc, pos);
         taTestReport.testCaseInfo = ttc.getLogString(); 
+        taTestReport.message += (" BetterResults=[" + taTestReport.betterResults.toString() + "]");
         return taTestReport;
     }
 
@@ -75,7 +76,8 @@ public class TaTestReport {
         List<String> betterResults = new ArrayList<String>();
         List<Typeahead> resultList = taTestCase.getResults();
         Typeahead t;
-        for (int i = 0; i < resultList.size() && i < pos - 1; i++) {
+        int endIndex = (pos == 0 ? resultList.size() : pos);
+        for (int i = 0; i < endIndex ; i++) {
             t = resultList.get(i);
             betterResults.add("(" + t.getDisplayText() + ")--(" + t.getId() + ")");
         }
